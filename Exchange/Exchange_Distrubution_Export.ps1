656f6eccb56f87a4ef5b0213434a3ed3b1fb3d74
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-27 08:24:31 -0600

    Updated script

--- Exchange/Exchange_Distrubution_Export.ps1
+++ Exchange/Exchange_Distrubution_Export.ps1
@@ -6,7 +6,8 @@ Source:
 Description: Export distrubtion group with members in CSV.
 #>
 
+# Change UPN to your global admin account.
 Connect-ExchangeOnline -UserPrincipalName contoso@domain.com 
 
-#Change Distrubtion group name to whichever one needing exportation. 
+# Change Distrubtion group name to whichever one needing exportation. 
 Get-DistributionGroupMember -Identity 'NameOfGroup' | Select-Object Name, PrimarySmtpAddress | Export-csv C:\Users\<Username>\ExchDist.csv -NoTypeInformation

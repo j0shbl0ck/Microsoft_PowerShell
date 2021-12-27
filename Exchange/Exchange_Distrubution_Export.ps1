a517f2287a3e1a9ae24bb36ef423f0502a16b001
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-27 08:23:18 -0600

    Added new PowerShell script and folder.

---
+++ Exchange/Exchange_Distrubution_Export.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Author: J Block
+Type: Public
+Date: 12.27.21
+Source: 
+Description: Export distrubtion group with members in CSV.
+#>
+
+Connect-ExchangeOnline -UserPrincipalName contoso@domain.com 
+
+#Change Distrubtion group name to whichever one needing exportation. 
+Get-DistributionGroupMember -Identity 'NameOfGroup' | Select-Object Name, PrimarySmtpAddress | Export-csv C:\Users\<Username>\ExchDist.csv -NoTypeInformation

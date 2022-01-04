4cecea8c1cd9a50a4cb29fced51f8426b681be0d
Josh Block <247jblock@gmail.com>
2022-01-04 13:39:27 -0600

    Added new folder, uploaded PowerShell script

---
+++ Manage Users, Groups, and Passwords/User_Password_Never_Expire.ps1
@@ -0,0 +1,33 @@
+﻿<#
+    .NOTES
+    =============================================================================
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.1
+    Date: 01.04.22
+    Type: Public
+    Source: https://docs.microsoft.com/en-us/microsoft-365/admin/add-users/set-password-to-never-expire?view=o365-worldwide
+    Description: This script is made set the password of a user to never expire. 
+    =============================================================================
+    .ADDITIONAL NOTES
+        You will need to have AzureAD PowerShell module
+    =============================================================================
+    .EXAMPLE
+    attrib +s +h "C:\Users\Public\Desktop\Google Chrome.lnk" | To Hide
+    attrib -s -h "C:\Users\Public\Desktop\Google Chrome.lnk" | To Unhide
+#>
+
+
+Connect-AzureAD
+
+# In the qoutes, type in the UPN (for example, user@contoso.onmicrosoft.com).
+$User_UPN = "user@contoso.onmicrosoft.com"
+
+# This sets the password of one user to never expire
+Set-AzureADUser -ObjectId $User_UPN -PasswordPolicies DisablePasswordExpiration
+
+#This shows a confirmation of whether the password is set to never expire
+Get-AzureADUser -ObjectId $User_UPN | Select-Object UserprincipalName,@{
+    N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
+}
+
+Pause

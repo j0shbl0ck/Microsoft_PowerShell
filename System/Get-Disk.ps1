14adc6203e7ddd14c7771027e1535c2d1250d447
Josh Block <247jblock@gmail.com>
2022-01-11 14:44:55 -0600

    Updated script

---
+++ System/Get-Disk.ps1
@@ -0,0 +1,14 @@
+<#
+    .NOTES
+    =============================================================================
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 01.11.22
+    Type: Public
+    Source: 
+    Description: This script produces the devices serial number. 
+    =============================================================================
+#>
+
+# Run PowerShell as adminstrator to receive information. 
+Get-Disk

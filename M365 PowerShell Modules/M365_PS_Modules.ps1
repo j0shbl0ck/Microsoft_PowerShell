32c3ed4d738f7ce558fbc374aa80362257dae6dd
Josh Block <247jblock@gmail.com>
2022-01-12 11:59:32 -0600

    Uploaded script

---
+++ M365 PowerShell Modules/M365_PS_Modules.ps1
@@ -0,0 +1,29 @@
+# To be able to execute scripts, if not already performed
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name PowerShellGet -Force -AllowClobber
+
+# Installs Exchange Powershell Module
+Install-Module -Name ExchangeOnlineManagement
+
+# Installs SharePoint Online Powershell Module
+Install-Module -Name PowerShellGet -Force -AllowClobber
+
+Install SharePoint PNP
+Install-Module PnP.PowerShell
+
+# Install AzureAD V1 Powershell Module
+Install-Module -Name MSOnline
+
+# Install AzureAD V2 PowerShell Module
+Install-Module -Name AzureAD
+
+# Install Microsoft Intune PowerShell Module
+Install-Module Microsoft.Graph.Intune
+## Required for first time use (uncomment and run in seperate PS session)
+# Connect-MSGraph -AdminConsent
+
+# Install Autopilot Diagnostics
+Install-Script Get-AutopilotDiagnostics -Force
+
+# Install Teams PowerShell Module
+Install-Module -Name MicrosoftTeams -Force -AllowClobber

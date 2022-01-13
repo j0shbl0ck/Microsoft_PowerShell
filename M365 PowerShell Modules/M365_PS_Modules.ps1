939e2aa38f94a9a7178b80b9735dac3a22b1a86e
Josh Block <247jblock@gmail.com>
2022-01-12 21:29:46 -0600

    Moved comments to ".NOTES"

--- M365 PowerShell Modules/M365_PS_Modules.ps1
+++ M365 PowerShell Modules/M365_PS_Modules.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+    This script installs the M365 Powershell Module Services.
+.DESCRIPTION
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 01.12.22
+    Type: Public
+.NOTES
+    You will need accept the "Untrusted Repository" prompts.
+    To use Microsoft Graph Intune, you will need to run for the first time:  Connect-MSGraph -AdminConsent 
+.LINK
+    Source: https://o365reports.com/2019/11/01/install-all-office-365-powershell-modules/
+#>
+
 # To be able to execute scripts, if not already performed
 Set-ExecutionPolicy RemoteSigned
 Install-Module -Name PowerShellGet -Force -AllowClobber
@@ -19,11 +34,11 @@ Install-Module -Name AzureAD
 
 # Install Microsoft Intune PowerShell Module
 Install-Module Microsoft.Graph.Intune
-## Required for first time use (uncomment and run in seperate PS session)
-# Connect-MSGraph -AdminConsent
 
 # Install Autopilot Diagnostics
 Install-Script Get-AutopilotDiagnostics -Force
 
 # Install Teams PowerShell Module
 Install-Module -Name MicrosoftTeams -Force -AllowClobber
+
+Pause

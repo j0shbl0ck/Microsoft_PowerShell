45e599c04ac5d3fc9fc71f3c06cfe288bfb09bfd
Josh Block <247jblock@gmail.com>
2022-01-15 13:36:21 -0600

    Moved "Complete!" to end of script

--- Manage Users, Groups, and Passwords/User_Password_Never_Expire.ps1
+++ Manage Users, Groups, and Passwords/User_Password_Never_Expire.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.5
+    Version: 1.0.6
     Date: 01.04.22
     Type: Public
     Source: https://docs.microsoft.com/en-us/microsoft-365/admin/add-users/set-password-to-never-expire?view=o365-worldwide
@@ -23,12 +23,12 @@ Start-Sleep -s 5
 # This sets the password of one user to never expire
 Write-Host 'Disabling password experiation...' -ForegroundColor Yellow
 Set-AzureADUser -ObjectId $User_UPN -PasswordPolicies DisablePasswordExpiration
-Write-Host 'Complete!' -ForegroundColor Yellow
 
 #This shows a confirmation of whether the password is set to never expire
 Write-Host '======= User Password Policy  =======' -ForegroundColor Yellow
 Get-AzureADUser -ObjectId $User_UPN | Select-Object UserprincipalName,@{
     N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
 }
+Write-Host 'Complete!' -ForegroundColor Yellow
 
 Start-Sleep -s 30

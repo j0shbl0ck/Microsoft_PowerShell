43427785a6a04ed1ba5da04daeeec10ca5bebea8
Josh Block <247jblock@gmail.com>
2022-01-15 13:32:20 -0600

    Updated Start-Sleep numbers

--- Manage Users, Groups, and Passwords/User_Password_Never_Expire.ps1
+++ Manage Users, Groups, and Passwords/User_Password_Never_Expire.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.4
+    Version: 1.0.5
     Date: 01.04.22
     Type: Public
     Source: https://docs.microsoft.com/en-us/microsoft-365/admin/add-users/set-password-to-never-expire?view=o365-worldwide
@@ -18,7 +18,7 @@ Connect-AzureAD
 $User_UPN = Read-Host -Prompt 'Input User (enduser@domain.com) to disable password expiration'
 # ======= VARIABLES ======= #
 
-Start-Sleep 5s
+Start-Sleep -s 5
 
 # This sets the password of one user to never expire
 Write-Host 'Disabling password experiation...' -ForegroundColor Yellow
@@ -31,4 +31,4 @@ Get-AzureADUser -ObjectId $User_UPN | Select-Object UserprincipalName,@{
     N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
 }
 
-Start-Sleep 30s
+Start-Sleep -s 30

06617fdb04712148f8881f25b14ecfde55a5b036
Josh Block <247jblock@gmail.com>
2022-01-17 09:40:45 -0600

    Updated Function and added additional comments

--- Exchange Online/Change_Calendar_Rights.ps1
+++ Exchange Online/Change_Calendar_Rights.ps1
@@ -3,7 +3,7 @@
     This script allows you to view calendar permissions through Exchange Online PowerShell
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.6
+    Version: 1.0.7
     Date: 01.17.22
     Type: Public
 .EXAMPLE
@@ -23,31 +23,41 @@ $seconduser = Read-Host -Prompt 'Input user email (seconduser@domain.com) of who
 Connect-ExchangeOnline
 
 # Displays Outlook calendar permission levels and access roles.
-
-Write-Host  "" -ForegroundColor Yellow
-Write-Host "Outlook Calendar Permission Levels and Access Roles" -ForegroundColor Yellow
-Write-Host "=======================================================" -ForegroundColor Yellow
+function Write-Role {
+Write-Host -ForegroundColor Yellow "" 
+Write-Host -ForegroundColor Yellow "Outlook Calendar Permission Levels and Access Roles"
+Write-Host -ForegroundColor Yellow "======================================================="
 Write-Host -ForegroundColor Yellow "Owner — gives full control of the mailbox folder: read, create, modify, and delete all items and folders. Also, this role allows to manage item's permissions"
 Write-Host -ForegroundColor Yellow "PublishingEditor — read, create, modify, and delete items/subfolders (all permissions, except the right to change permissions)"
 Write-Host -ForegroundColor Yellow "Editor — read, create, modify, and delete items (can't create subfolders)"
-Write-Host -ForegroundColor Yellow "PublishingAuthor — create, read all items/subfolders. You can modify and delete only items you create"
+Write-Host -ForegroundColor Yellow "PublishingAuthor — create, read all items/subfolders. You can modify and delete only items you create."
 Write-Host -ForegroundColor Yellow "Author — create and read items. Edit and delete own items"
 Write-Host -ForegroundColor Yellow "NonEditingAuthor — full read access, and create items. You can delete only your own items"
 Write-Host -ForegroundColor Yellow "Reviewer — read folder items only"
-Write-Host -ForegroundColor Yellow "Contributor — create items and folders (can't read items)"
+Write-Host -ForegroundColor Yellow "Contributor — create items and folders (can't read items)."
 Write-Host -ForegroundColor Yellow "AvailabilityOnly — read Free/Busy info from the calendar"
 Write-Host -ForegroundColor Yellow "LimitedDetails — view availability data with calendar item subject and location"
-Write-Host -ForegroundColor Yellow "None — no permissions to access folder and files."    
+Write-Host -ForegroundColor Yellow "None — no permissions to access folder and files."
+Write-Host -ForegroundColor Yellow "======================================================="
+Write-Host -ForegroundColor Yellow ""
+}    
 
+Write-Role;
 
 
 $role = Read-Host -Prompt 'Input access role you wish to give second user to main users calendar'
 Add-MailboxFolderPermission -Identity ${mainuser}:\calendar -user $seconduser -AccessRights $role
-Write-Verbose = "Allowing $seconduser the role of $role to $mainuser calendar..." -ForegroundColor Cyan
-Write-Verbose = "Complete!" -ForegroundColor Cyan
-Write-Host = ""
-Write-Host '======= Calendar Rights Other Users Have to Main User  =======' -ForegroundColor Yellow
+
+# Outputs conformation of adding calendar permissions.=
+Write-Host -ForegroundColor Cyan "Allowing $seconduser the role of $role to $mainuser calendar..."
+Write-Host -ForegroundColor Cyan "Complete!"
+Write-Host ""
+
+# Shows other user rights to $mainuser
+Write-Host -ForegroundColor Yellow "======= Calendar Rights Other Users Have to $mainuser =======" 
 Get-EXOMailboxFolderPermission -Identity ${mainuser}:\calendar
+
+Pause
 
 
 

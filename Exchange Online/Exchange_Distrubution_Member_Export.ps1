3e9bcead5a63d8bd150e72947a9967a8e53a7697
Josh Block <247jblock@gmail.com>
2022-01-17 14:42:32 -0600

    Complete overhaul to script in adding prompts and comments

--- Exchange Online/Exchange_Distrubution_Member_Export.ps1
+++ Exchange Online/Exchange_Distrubution_Member_Export.ps1
@@ -3,17 +3,31 @@
     Export distrubtion group with members in CSV. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 01.04.22
     Type: Public
 .NOTES
     You will need to have AzureAD PowerShell module [ Install-Module -Name AzureAD ]
+    You will need to have Exchange Online module [ Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5 ]
+    
 .LINK
-    Source: https://docs.microsoft.com/en-us/microsoft-365/admin/add-users/set-password-to-never-expire?view=o365-worldwide
+    Source: https://www.datarepairtools.com/blog/export-office-365-distribution-group-members-list-to-csv/
 #>
 
 # Change UPN to your Global/Exchange admin account.
-Connect-ExchangeOnline -UserPrincipalName admin@domain.com 
+Connect-ExchangeOnline
 
-# Change Distrubtion group name to whichever one needing exportation. 
-Get-DistributionGroupMember -Identity 'NameOfGroup' | Select-Object Name, PrimarySmtpAddress | Export-csv C:\O365\ExchDist.csv -NoTypeInformation
+# Get Distrubution Group Email
+$groupname = Read-Host -Prompt "Enter the email of the distrubution group"
+
+# Exportation section of the script
+Write-Host -ForegroundColor Yellow "Creating ${groupname} CSV file..."
+Get-DistributionGroupMember -Identity $groupname | Select-Object Name, PrimarySmtpAddress | Export-CSV "$($env:USERPROFILE)\Desktop\$groupname.csv"  -NoTypeInformation -ErrorAction Stop
+Write-Host -ForegroundColor Green "Complete! Document found at Desktop\${groupname}.csv"
+
+# Terminates Exchange Online PS Session
+Write-Host 'Terminating Exchange Online PS Session...' -ForegroundColor Cyan
+Write-Host ""
+Disconnect-ExchangeOnline -Confirm:$false
+
+Pause

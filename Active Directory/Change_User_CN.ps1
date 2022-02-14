b92b4d30835e5caa7481c4486b1b2ab54b60d3d2
Josh Block <247jblock@gmail.com>
2022-02-14 09:13:39 -0600

    Minor patch fixes 🔨

--- Active Directory/Change_User_CN.ps1
+++ Active Directory/Change_User_CN.ps1
@@ -3,7 +3,7 @@
     This script changes the CN of the ADObject
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 01.19.22
     Type: Public
 .NOTES
@@ -17,14 +17,20 @@ $username = Read-Host -Prompt "Enter username (SamAccountName attribute) of ADOb
 
 # Pulls ADUser information primarily objectGUID and displayName
 $information = Get-ADUser $username -Properties *
+Write-Host -ForegroundColor Cyan "Pulling ADUser information from objectGUID and displayName..."
 
 # Changes ADOBject CN to current displayName Attribute
 Rename-ADObject -Identity $information.objectGUID -NewName $information.displayName
+Write-Host -ForegroundColor Cyan "Changing ADObject CN to current displayName atttribute..."
 
 # Retrives new CN attribute
 $changedname = Get-ADUser -Identity $information.objectGUID
+Write-Host ""
 
-Write-Host -ForegroundColor Yellow "UserObject:" $changedname
+Write-Host -ForegroundColor Yellow "distinguishedName:" $changedname
 Write-Host -ForegroundColor Yellow "Old Name:" $information.Name
 Write-Host -ForegroundColor Yellow "New Name:" $changedname.Name
 
+# Wait for end-user to close session
+Write-Host -ForegroundColor Green "Change Complete! You may close this session."
+Start-Sleep -Seconds 15 

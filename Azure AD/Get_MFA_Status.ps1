8cee4b1a54255c6f0660de61e2538bf2838fa3f3
Josh Block <247jblock@gmail.com>
2022-03-23 11:25:07 -0500

    ⚒️  updated Powershell Console Window

--- Azure AD/Get_MFA_Status.ps1
+++ Azure AD/Get_MFA_Status.ps1
@@ -3,7 +3,7 @@
     This script pulls information on whether a user has MFA enabled or not. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 03.23.22
     Type: Public
 .NOTES
@@ -12,7 +12,7 @@
     Source: https://dailysysadmin.com/KB/Article/3725/use-powershell-to-get-the-mfa-enabled-or-disabled-status-of-office-365-and-azure-users-and-type-of-mfa-used/
 #>
 
-start-process powershell '-noexit -command "[console]::windowwidth=100; [console]::windowheight=50; [console]::bufferwidth=[console]::windowwidth"'
+start-process powershell '-noexit -command "[console]::windowwidth=150; [console]::windowheight=100; [console]::bufferwidth=[console]::windowwidth"'
 
 Connect-MsolService
 

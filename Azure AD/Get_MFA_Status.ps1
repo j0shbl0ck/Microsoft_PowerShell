2cda298a62da22f2b6de8250bfc552eb18760bad
Josh Block <247jblock@gmail.com>
2022-03-23 11:43:03 -0500

    ⚒️  added close session

--- Azure AD/Get_MFA_Status.ps1
+++ Azure AD/Get_MFA_Status.ps1
@@ -3,7 +3,7 @@
     This script pulls information on whether a user has MFA enabled or not. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 03.23.22
     Type: Public
 .NOTES
@@ -17,5 +17,8 @@ Connect-MsolService
 
 # Retrives MFA status per user
 Get-MsolUser -all | select DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where IsDefault -eq $True).MethodType} else { "Disabled"}}} | FT -AutoSize
+
+# End MS Online session
+[Microsoft.Online.Administration.Automation.ConnectMsolService]::ClearUserSessionState()
 
 Pause 

8e22be189ab06a6b56c04eae3f938c761c58b515
Josh Block <247jblock@gmail.com>
2022-03-25 16:36:38 -0500

    🔨  fixed script

--- Azure AD/AAD_Connect_DeltaSync.ps1
+++ Azure AD/AAD_Connect_DeltaSync.ps1
@@ -3,7 +3,7 @@
     This script performs a delta sync on Azure AD Connect. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 02.10.22
     Type: Public
 .NOTES
@@ -14,6 +14,9 @@
 
 # Imports ADSync Module
 Import-Module ADSync
+
+## Remote PowerShell Connection to Azure AD Connect Server (Uncomment below to use)
+#Enter-PSSession [ADConnect Server]
 
 # Shows current schedule settings for ADSync
 Get-ADSyncSchedule

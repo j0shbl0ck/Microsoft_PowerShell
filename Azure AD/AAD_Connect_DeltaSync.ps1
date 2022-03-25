2e0da1075985f5f264d621d0754c394dd52b3606
Josh Block <247jblock@gmail.com>
2022-03-25 16:42:55 -0500

    Updated version of script

--- Azure AD/AAD_Connect_DeltaSync.ps1
+++ Azure AD/AAD_Connect_DeltaSync.ps1
@@ -3,7 +3,7 @@
     This script performs a delta sync on Azure AD Connect. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 02.10.22
     Type: Public
 .NOTES

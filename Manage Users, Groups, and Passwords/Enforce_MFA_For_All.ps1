f956d32b814a787cb4ccccdb4ab8d7b5c8eabc2f
Josh Block <247jblock@gmail.com>
2022-03-29 14:13:52 -0500

    🔨  updated script

--- Manage Users, Groups, and Passwords/Enforce_MFA_For_All.ps1
+++ Manage Users, Groups, and Passwords/Enforce_MFA_For_All.ps1
@@ -3,43 +3,22 @@
     This script enforces MFA for all users within the tenant
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 03.29.22
     Type: Public
 .NOTES
-
+    Ensure you make note of your global/service accounts that you will want to disable MFA after the script runs.
 .LINK
     https://www.c-sharpcorner.com/blogs/how-to-enable-and-disable-mfa-using-powershell
 #>
 
 # Connect MSOnline
-$cred = New-Object System.Management.Automation.PSCredential("infinitech@basilmcrae.com", "wmGmEWqx3JpQpX_rJdXLQB8Cm6sw1XYS")
 Connect-MsolService 
 
 # Make status to "Enforced"
 $st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
 $st.RelyingParty = "*"
-$st.State = "Enforced" 
+$st.State = "Enabled" 
 $sta = @($st)
-Set-MsolUser -UserPrincipalName $upn -StrongAuthenticationRequirements $sta -StrongAuthenticationMethods $sm
-Get-MsolUser –All | Foreach{ Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $sta}
-# this script works above 
+Get-MsolUser -All | Foreach{ Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $sta}
 
-# Connect MSOnline
-
-
-$UserName=Read-Host"Enter the username"  
-$authentication=New-Object -TypeNameMicrosoft.Online.Administration.StrongAuthenticationRequirement  
-$authentication.RelyingParty ="*"  
-$authentication.State ="Enforced"  
-Get-MsolUser –All | Foreach{ Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $authentication}
-
-
-Connect-MSolservice
-$upn = "0119@zzjtest.onmicrosoft.com"       #(Target user, if you have csv file of target users, you may re-write the script to go through the UPNs from csv file)
-$st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
-$st.RelyingParty = "*"
-$st.State = "Enforced" 
-$sta = @($st)
-Set-MsolUser -UserPrincipalName $upn -StrongAuthenticationRequirements $sta -StrongAuthenticationMethods $sm
-Get-MsolUser –All | Foreach{ Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $sta}

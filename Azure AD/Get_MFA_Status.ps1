67f20869b8bfa3e86b41a786b6e7b264c689ff87
Josh Block <247jblock@gmail.com>
2022-03-30 09:07:02 -0500

    🔨  Added option to remove MFA status

--- Azure AD/Get_MFA_Status.ps1
+++ Azure AD/Get_MFA_Status.ps1
@@ -3,7 +3,7 @@
     This script pulls information on whether a user has MFA enabled or not. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.5
+    Version: 1.0.6
     Date: 03.23.22
     Type: Public
 .NOTES
@@ -16,7 +16,11 @@
 Connect-MsolService
 
 # Retrives MFA status per user
-Get-MsolUser -all | select DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where IsDefault -eq $True).MethodType} else { "Disabled"}}} | FT -AutoSize
+Get-MsolUser -all | Select-Object DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where-Object IsDefault -eq $True).MethodType} else { "Disabled"}}} | Format-Table -AutoSize
+
+## Remove MFA from user
+#Get-MsolUser -all | Select-Object DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where-Object IsDefault -eq $True).MethodType} else { "Disabled"}}} | Format-Table -AutoSize | Where-Object MFAStatus -eq "Disabled" | Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationMethods @()
+#Set-MsolUser -UserPrincipalName username@your_tenant.onmicrosoft.com -StrongAuthenticationMethods @()
 
 # End MS Online session
 [Microsoft.Online.Administration.Automation.ConnectMsolService]::ClearUserSessionState()

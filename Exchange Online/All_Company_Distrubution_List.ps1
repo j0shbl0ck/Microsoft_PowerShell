43a54a0949193d2a64caf982e1006aaa19f2eaae
Josh Block <247jblock@gmail.com>
2022-04-14 18:52:14 -0500

    ✅  Script fully developed

--- Exchange Online/All_Company_Distrubution_List.ps1
+++ Exchange Online/All_Company_Distrubution_List.ps1
@@ -3,13 +3,14 @@
     This script gets every user excluding unlicensed and external then adds them to an all company list.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.4
+    Version: 1.0.5
     Date: 04.14.22
     Type: Public
 .EXAMPLE
 .NOTES
     You will need to have MSOnline PowerShell module [ Install-Module -Name MSOnline ]
     You will need to have Exchange Online module [ Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5 ]
+    You will need to sign in twice for each Azure connection.
     After distribution list is created, check online for the distribution list name and possible primary email address change.
 .LINK
     https://social.technet.microsoft.com/Forums/ie/en-US/a48b455e-0114-424f-8b0f-a8c0b88dfb0f/exchange-powershell-loop-through-all-usersmailboxes-and-run-an-exchange-command-on-the-mailbox?forum=winserverpowershell
@@ -26,6 +27,8 @@ Connect-MsolService
 
 # Create all company distrubition list.
 New-DistributionGroup -Name "AllCompany" -Type "Distribution" 
+Write-Host ""
+Write-Host "Primary Smtp Address can be changed online if current domain name not desired." -ForegroundColor Yellow
 
 # Get all users excluding unlicensed and external
 $user = Get-MsolUser -All | 
@@ -38,8 +41,13 @@ $user | ForEach-Object {
     Add-DistributionGroupMember -Identity "AllCompany" -Member $_.UserPrincipalName
 }
 
+Write-Host "All members of company list below:" -ForegroundColor Cyan
+
 # Show members of all company list.
-Get-DistributionGroupMember -Identity "AllCompany"
+Get-DistributionGroupMember -Identity "AllCompany" | 
+    Select-Object DisplayName, PrimarySmtpAddress |
+    Sort-Object DisplayName, PrimarySmtpAddress |
+    Format-Table -AutoSize 
 
 # In green, show success
 Write-Host "Distribution group created successfully" -ForegroundColor Green
@@ -51,4 +59,4 @@ Pause
 Write-Host "Script ended" -ForegroundColor Magenta
 
 # Disconnect from Exchange Online
-Disconnect-ExchangeOnline
+Disconnect-ExchangeOnline -Confirm:$false

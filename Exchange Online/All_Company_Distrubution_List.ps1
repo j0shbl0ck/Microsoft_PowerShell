3861718a8f92073ecd9715f8dd9e9409c7bbfc94
Josh Block <247jblock@gmail.com>
2022-04-14 18:12:09 -0500

    ✨  polishing script

--- Exchange Online/All_Company_Distrubution_List.ps1
+++ Exchange Online/All_Company_Distrubution_List.ps1
@@ -3,13 +3,14 @@
     This script gets every user excluding unlicensed and external then adds them to an all company list.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 04.14.22
     Type: Public
 .EXAMPLE
 .NOTES
     You will need to have MSOnline PowerShell module [ Install-Module -Name MSOnline ]
     You will need to have Exchange Online module [ Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5 ]
+    After distribution list is created, check online for the distribution list name and possible primary email address change.
 .LINK
     https://social.technet.microsoft.com/Forums/ie/en-US/a48b455e-0114-424f-8b0f-a8c0b88dfb0f/exchange-powershell-loop-through-all-usersmailboxes-and-run-an-exchange-command-on-the-mailbox?forum=winserverpowershell
     https://medium.com/@writeabednego/bulk-create-and-add-members-to-distribution-lists-and-shared-mailboxes-using-powershell-89f5ef6e1362
@@ -36,3 +37,18 @@ $user = Get-MsolUser -All |
 $user | ForEach-Object {
     Add-DistributionGroupMember -Identity "AllCompany" -Member $_.UserPrincipalName
 }
+
+# Show members of all company list.
+Get-DistributionGroupMember -Identity "AllCompany"
+
+# In green, show success
+Write-Host "Distribution group created successfully" -ForegroundColor Green
+
+# Pause script
+Pause
+
+# In purple, show script end
+Write-Host "Script ended" -ForegroundColor Magenta
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline

32511d0c17d65ad855567eb9c4b570e35a05a03f
Josh Block <247jblock@gmail.com>
2022-04-15 16:37:42 -0500

    🔨  Added additional functionality in retriving hostname

--- Active Directory/AAD_Connect_Server.ps1
+++ Active Directory/AAD_Connect_Server.ps1
@@ -3,7 +3,7 @@
     This script pulls information on where Azure AD Connect is installed
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 03.23.22
     Type: Public
 .NOTES
@@ -15,10 +15,20 @@
 # Enter global admin credentials
 Connect-MsolService
 
+
 # Retrives AD Connect Hostname
-(Get-MsolCompanyInformation).DirSyncClientMachineName
+$hostname = (Get-MsolCompanyInformation).DirSyncClientMachineName
+Write-Host -ForegroundColor Yellow $hostname
+Write-Host ""
+
+# if no hostname is found, output error message
+if ($null -eq $hostname) {
+    Write-Host -ForegroundColor Red "No AD Connect Hostname Found"
+    Write-Host ""
+}
 
 # End MS Online session
 [Microsoft.Online.Administration.Automation.ConnectMsolService]::ClearUserSessionState()
 
 Pause 
+Write-Host "Exiting..."

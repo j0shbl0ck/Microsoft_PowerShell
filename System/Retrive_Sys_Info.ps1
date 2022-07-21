04df79cb971f8e3a54e6773fc2fa15e0645d6b04
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-07-20 22:05:07 -0500

    🔨  added network information

--- System/Retrive_Sys_Info.ps1
+++ System/Retrive_Sys_Info.ps1
@@ -3,7 +3,7 @@
     This script allows you view system information of the device.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 02.07.22
     Type: Public
 .NOTES
@@ -12,7 +12,7 @@
 
 # === VARIABLES START === #
 $hostname = hostname
-$userinfo = dir env:user*
+$userinfo = Get-ChildItem env:user*
 $disk = Get-PhysicalDisk
 $volume = Get-Volume
 # === VARIABLES END === #
@@ -31,9 +31,6 @@ Get-CimInstance -ClassName win32_bios
 Write-Host -ForegroundColor Cyan "COMPUTER SYSTEM:"
 Get-CimInstance -ClassName Win32_ComputerSystem
 
-Write-Host -ForegroundColor Cyan "SYSTEM PROCESSOR:"
-Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
-
 Write-Host -ForegroundColor Cyan "OS INFORMATION:"
 Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property Build*,OSType
 
@@ -41,6 +38,12 @@ Write-Host -ForegroundColor Cyan "DISK INFORMATION"
 Write-Output $disk
 Write-Output "========================="
 Write-Output $volume
+
+Write-Host -ForegroundColor Cyan "SYSTEM PROCESSOR:"
+Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
+
+Write-Host -ForegroundColor Cyan "NETWORK INFORMATION:"
+Get-NetIPConfiguration
 
 Write-Host -ForegroundColor Magenta "All information produced. Close PowerShell when ready..."
 [void][System.Console]::ReadKey($FALSE)

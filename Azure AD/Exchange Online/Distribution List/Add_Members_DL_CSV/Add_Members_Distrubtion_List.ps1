a6c4f0e4849b304d54846f0d94c5a11d21671a3d
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-07-21 09:29:25 -0500

    ✨  polishing script

--- Azure AD/Exchange Online/Distribution List/Add_Members_DL_CSV/Add_Members_Distrubtion_List.ps1
+++ Azure AD/Exchange Online/Distribution List/Add_Members_DL_CSV/Add_Members_Distrubtion_List.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 07.21.22
     Type: Public
-    Version: 1.0.1
+    Version: 1.0.2
 .LINK
     https://github.com/j0shbl0ck
     https://social.technet.microsoft.com/wiki/contents/articles/54249.365-add-members-in-distribution-list-using-powershell-and-csv-list-file.aspx
@@ -14,13 +14,12 @@
 
 # Connect to Exchange Online via Azure AD with Global/Exchange admin.
 Write-Host -ForegroundColor Yellow 'Connecting to Exchange Online...'
-Import-Module ExchangeOnline
 Connect-ExchangeOnline
 Write-Host -ForegroundColor Green 'Connected to Exchange Online.'
 Write-host ""
 
 # Ask user for file path to .CSV
-Write-Host -ForegroundColor Yellow 'Please enter the path to the .CSV file:'
+Write-Host -ForegroundColor Yellow 'Please enter the path (no quotes around path) to the .CSV file:'
 $filePath = Read-Host
 Write-Host -ForegroundColor Green 'File path entered.'
 Write-host ""
@@ -31,7 +30,7 @@ Write-Host -ForegroundColor Green 'Import complete.'
 Write-host ""
 
 # Ask user for Distribution List email address
-Write-Host -ForegroundColor Yellow 'Please enter the email address of the Distribution List:'
+Write-Host -ForegroundColor Yellow 'Please enter the email address (no quotes around email) of the Distribution List:'
 $distList = Read-Host
 Write-Host -ForegroundColor Green 'Distribution List email address entered.'
 Write-host ""
@@ -42,10 +41,18 @@ Import-CSV $filePath | ForEach-Object {Add-DistributionGroupMember -Identity $di
 Write-Host -ForegroundColor Green 'Members added to distribution list.'
 Write-host ""
 
+# Get the distribution list members
+Write-Host -ForegroundColor Yellow 'Getting distribution list members...'
+Get-DistributionGroupMember -Identity $distList
+
 # Give user a chance to see the results
-Write-Host -ForegroundColor Yellow 'Please press enter to continue...'
+Write-Host -ForegroundColor Yellow 'Press any key to exit...'
 Read-Host
 Write-Host -ForegroundColor Green 'Exiting...'
+
+# Disconnect from Exchange Online
+Write-Host -ForegroundColor Yellow 'Disconnecting from Exchange Online...'
+Disconnect-ExchangeOnline
 Write-Host -ForegroundColor Green 'Done.'
 Write-host ""
 

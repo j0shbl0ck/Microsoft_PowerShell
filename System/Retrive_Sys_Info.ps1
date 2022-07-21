e35014a17b1dd376f004b63dcfa6324f252d07a2
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-07-20 23:00:20 -0500

    🛠️ Added additional information to find on device

--- System/Retrive_Sys_Info.ps1
+++ System/Retrive_Sys_Info.ps1
@@ -3,7 +3,7 @@
     This script allows you view system information of the device.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 02.07.22
     Type: Public
 .NOTES
@@ -12,9 +12,6 @@
 
 # === VARIABLES START === #
 $hostname = hostname
-$userinfo = Get-ChildItem env:user*
-$disk = Get-PhysicalDisk
-$volume = Get-Volume
 # === VARIABLES END === #
 
 # Informational Message
@@ -22,28 +19,29 @@ Write-Host -ForegroundColor Yellow "This entails all information for device: ${h
 Write-Host ""
 
 Write-Host -ForegroundColor Cyan "CURRENT USER INFORMATION:"
-Write-Output $userinfo
+Write-Host ""
+Write-Host 'Domain\Username:' ((Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty username))
 Write-Host ""
 
 Write-Host -ForegroundColor Cyan "BIOS INFORMATION:"
-Get-CimInstance -ClassName win32_bios
+Get-CimInstance -ClassName win32_bios | Format-Table -AutoSize
 
 Write-Host -ForegroundColor Cyan "COMPUTER SYSTEM:"
-Get-CimInstance -ClassName Win32_ComputerSystem
+Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Name,PrimaryOwnerName,Domain,Model,Manufacturer, `
+@{N='RAM(GB)'; E={[Math]::Round(($_.TotalPhysicalMemory)/1GB,2)}} | Format-Table -AutoSize
 
 Write-Host -ForegroundColor Cyan "OS INFORMATION:"
-Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property Build*,OSType
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property Build*,OSType | Format-Table -AutoSize
 
 Write-Host -ForegroundColor Cyan "DISK INFORMATION"
-Write-Output $disk
-Write-Output "========================="
-Write-Output $volume
+Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object{$_.DriveType -eq '3'} `
+| Select-Object DeviceID, VolumeName, `
+@{N='TotalSize(GB)'; E={[Math]::Round(($_.Size)/1GB,2)}}, `
+@{N='FreeSpace(GB)'; E={[Math]::Round(($_.FreeSpace)/1GB,2)}} | Format-Table -Autosize 
 
 Write-Host -ForegroundColor Cyan "SYSTEM PROCESSOR:"
-Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
+Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType | Format-Table -AutoSize
 
 Write-Host -ForegroundColor Cyan "NETWORK INFORMATION:"
-Get-NetIPConfiguration
+Get-NetIPConfiguration | Select-Object -Property InterfaceAlias,InterfaceDescription,NetProfile.Name,IPv4Address | Format-Table
 
-Write-Host -ForegroundColor Magenta "All information produced. Close PowerShell when ready..."
-[void][System.Console]::ReadKey($FALSE)

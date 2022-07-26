2ce84d4a1c3b3204ee9eefb77df03a8511038622
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-07-25 22:51:15 -0500

    ✨  polishing script

--- Manage Users, Groups, and Passwords/Create_Local_User.ps1
+++ Manage Users, Groups, and Passwords/Create_Local_User.ps1
@@ -3,7 +3,7 @@
     This script creates a localadmin account on the device. Use .\ to login into system.
     Author: Josh Block
 .NOTES
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 07.25.22
     Type: Public
 .LINK
@@ -16,18 +16,20 @@
 $ExpectedLocalUser = $(Write-Host "Enter a username for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host) 
 
 # Ask user for password
-$Password = Read-Host "Enter a password for the user account: "
+$Password = $(Write-Host "Enter a password for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
 $convertedpassword = ConvertTo-SecureString $Password -AsPlainText -Force
 
 # Ask user for full name
-$FullName = Read-Host "Enter the full name for the user account: "
+$FullName = $(Write-Host "Enter the full name for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
 
 # Ask user for description of account
-$Description = Read-Host "Enter a description for the user account: "
+$Description = $(Write-Host "Enter a description for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
 
 # Get local groups and ask user which one the user should be added to
 Get-LocalGroup | Format-Table -AutoSize 
-$localgroup = Read-Host "Enter the name of the local group the user should be added to: "
+$localgroup = $(Write-Host "Enter the name of the group the user should be added to: " -ForegroundColor yellow -NoNewLine; Read-Host)
+
+
 Write-Host ""
 
 Function Create_LocalAdmin
@@ -43,7 +45,7 @@ Try
 {
 
     ## Catch if not found
-    $LocaAdminUser = Get-LocalUser -Name $ExpectedLocalUser -ErrorAction Stop 
+    Get-LocalUser -Name $ExpectedLocalUser -ErrorAction Stop 
 
     ## If an account is found update the password
     Set-LocalUser -Name $ExpectedLocalUser -Password $convertedpassword -PasswordNeverExpires:$true

45914c30ba45c2432d5bd00759d930f1f9989b6e
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-07-30 18:47:45 -0500

    🔃  complete refresh of script

--- Manage Users, Groups, and Passwords/Create_Local_User.ps1
+++ Manage Users, Groups, and Passwords/Create_Local_User.ps1
@@ -3,89 +3,105 @@
     This script creates a localadmin account on the device. Use .\ to login into system.
     Author: Josh Block
 .NOTES
-    Version: 1.0.6
+    Version: 1.0.7
     Date: 07.25.22
     Type: Public
 .LINK
     https://github.com/j0shbl0ck
 #>
 
-==== ⚠️⚠️ SCRIPT UNDER DEVELOPMENT ⚠️⚠️ ====
-==== Needing to fix logic of entire output of script ====
-
-## Run this script in PowerShell ISE as admin to properly edit the script.
-
 # Ask user for username
-$ExpectedLocalUser = $(Write-Host "Enter a username for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host) 
 
-# check if user exists
-$ExpectedLocalUserExists = (Get-LocalUser $ExpectedLocalUser)
-# if user exists, ask user if they want to overwrite the user
-if ($ExpectedLocalUserExists) {
-    $OverwriteUser = $(Write-Host "User already exists. Overwrite user? (y/n): " -ForegroundColor yellow -NoNewLine; Read-Host)
-    if ($OverwriteUser -eq "y") {
-        # delete user
-        $ExpectedLocalUserExists.Delete()
-    } else 
-    # if user does not enter y or
+Clear-Host
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+$ObjLocalUser = $null
+
+#User to search for
+$USERNAME = $(Write-Host "Enter a username for the user account: " -ForegroundColor Cyan -NoNewLine; Read-Host) 
+
+try {
+    Write-Host "Searching for $($USERNAME) in User Accounts..." -ForegroundColor yellow
+    $ObjLocalUser = Get-LocalUser $USERNAME
+    $userfound = $(Write-Host "User $($USERNAME) was found! Reset $($USERNAME) password to continue? (y/n) " -ForegroundColor red -NoNewLine; Read-Host)
+    do {
+        if ($userfound -eq "y") {
+            # user to enter new password if user found
+            $Password = $(Write-Host "Enter a password for $($USERNAME): " -ForegroundColor yellow -NoNewLine; Read-Host)
+            $convertedpassword = ConvertTo-SecureString $Password -AsPlainText -Force
+            Write-Host "Resetting $($USERNAME) from User Accounts" -ForegroundColor yellow
+            Set-LocalUser -Name $USERNAME -Password $convertedpassword -PasswordNeverExpires:$true
+            Write-Host "User $($USERNAME) password was reset." -ForegroundColor green
+            Write-Host "See $($USERNAME) information below. " -ForegroundColor DarkGray
+            Get-LocalUser -Name $USERNAME | Select-Object *
+            # get groups the user is in
+            Write-Host "User $($USERNAME) is in the following groups: " -ForegroundColor DarkGray
+            foreach ($LocalGroup in Get-LocalGroup) {
+                if (Get-LocalGroupMember $LocalGroup -Member $USERNAME -ErrorAction SilentlyContinue)
+                {
+                $LocalGroup.Name
+                }
+            }
+            break
+        }
+    } until ($userfound -eq "n")
+
+}
+catch [Microsoft.PowerShell.Commands.UserNotFoundException] {
+    Write-Host "User $($USERNAME) was not found!" -ForegroundColor green | Write-Warning
+}
+catch {
+    "An unspecifed error occured" | Write-Error
+    Exit # Stop Powershell! 
+}
+
+#Create the user if it was not found (Example)
+if (!$ObjLocalUser) {
+    Write-Host "Creating User $($USERNAME)..." -ForegroundColor yellow
+    # Ask user for password
+    $Password = $(Write-Host "Enter a password for the user account: " -ForegroundColor Magenta -NoNewLine; Read-Host)
+    $convertedpassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+    # Ask user for full name
+    $FullName = $(Write-Host "Enter the full name for the user account: " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+    # Ask user for description of account
+    $Description = $(Write-Host "Enter a description for the user account: " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+    # Get local groups and ask user which one the user should be added to
+    Get-LocalGroup | Format-Table -AutoSize 
+    $localgroup = $(Write-Host "Enter the name of the group the user should be added to: " -ForegroundColor Magenta -NoNewLine; Read-Host)
+
+    # Create the user
+    New-LocalUser $USERNAME -Password $convertedpassword -FullName $FullName -Description $Description
+    Add-LocalGroupMember -Group $localgroup -Member $USERNAME
+    Set-LocalUser -Name $USERNAME -PasswordNeverExpires:$true
+    Write-Host ""
+
+    # Ask user if they would like to view user account in Local Users and Groups
+    $viewuser = $(Write-Host "User has been created. Would you like to view the user information in Local Users and Groups? (y/n): " -ForegroundColor Cyan -NoNewLine; Read-Host)
+    if ($viewuser -eq "y")
     {
-        # exit script
+        # open Local Users and Groups
+        Start-Process lusrmgr -Verb runas
+    } else {
+        # Show user information
+        Write-Host "See $($USERNAME) information below. " -ForegroundColor green
+        Get-LocalUser -Name $USERNAME | Select-Object *
+        # get groups the user is in
+        Write-Host "User $($USERNAME) is in the following groups: " -ForegroundColor DarkGray
+        foreach ($LocalGroup in Get-LocalGroup) {
+            if (Get-LocalGroupMember $LocalGroup -Member $USERNAME -ErrorAction SilentlyContinue)
+            {
+            $LocalGroup.Name
+            }
+        }
+        
+        # end the script
+        Write-Host ""
+        Write-Host "Press Any Key To Exit" -ForegroundColor DarkMagenta -NoNewLine; Read-Host
         exit
     }
-}
-
-# Ask user for password
-$Password = $(Write-Host "Enter a password for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
-$convertedpassword = ConvertTo-SecureString $Password -AsPlainText -Force
-
-# Ask user for full name
-$FullName = $(Write-Host "Enter the full name for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
-
-# Ask user for description of account
-$Description = $(Write-Host "Enter a description for the user account: " -ForegroundColor yellow -NoNewLine; Read-Host)
-
-# Get local groups and ask user which one the user should be added to
-Get-LocalGroup | Format-Table -AutoSize 
-$localgroup = $(Write-Host "Enter the name of the group the user should be added to: " -ForegroundColor yellow -NoNewLine; Read-Host)
-
-
-Write-Host ""
-
-Function Create_LocalAdmin
-{
-
-    New-LocalUser $ExpectedLocalUser -Password $convertedpassword -FullName $FullName -Description $Description
-    Add-LocalGroupMember -Group $localgroup -Member $ExpectedLocalUser
-    Set-LocalUser -Name $ExpectedLocalUser -PasswordNeverExpires:$true
-}
-
-Try
-
-{
-
-    ## Catch if not found
-    Get-LocalUser -Name $ExpectedLocalUser -ErrorAction Stop 
-
-    ## If an account is found update the password
-    Set-LocalUser -Name $ExpectedLocalUser -Password $convertedpassword -PasswordNeverExpires:$true
-
-}
-
-Catch
-
-{
-
-    Create_LocalAdmin
-
-}
-
-# Ask user if they would like to view user account in Local Users and Groups
-$viewuser = $(Write-Host "Would you like to view the user account in Local Users and Groups? (y/n): " -ForegroundColor Cyan -NoNewLine; Read-Host)
-if ($viewuser -eq "y")
-{
-    Start-Process lusrmgr -Verb runas
-} else {
-    Write-Host -ForegroundColor Green "User account created successfully."
 }
 
 

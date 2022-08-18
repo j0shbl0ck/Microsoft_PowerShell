82d6d5213ddb2d8e2b9cecf8ac0023e9eb3c8e9a
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-08-18 09:28:00 -0500

    ⬆️  uploaded new script

---
+++ Azure AD/Exchange Online/Bulk_Contact_Import.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    This creates imports contacts via CSV.
+.NOTES
+    Author: Josh Block
+    Date: 08.18.22
+    Type: Public
+    Version: 1.0.0
+.LINK
+    https://github.com/j0shbl0ck
+    https://m365scripts.com/exchange-online/bulk-import-contacts-office-365-powershell/#:~:text=Multiple%20contacts%20can%20be%20added,file%20with%20the%20contact%20info.
+#>
+
+Clear-Host
+
+# Connect to Exchange Online via Azure AD with Global/Exchange admin.
+Write-Host -ForegroundColor Cyan 'Connecting to Exchange Online...'
+Connect-ExchangeOnline
+Write-host ""
+
+# Ask user for file path to .CSV
+Write-Host -ForegroundColor Yellow 'Please enter the path (no quotes around path) to the .CSV file:'
+# Check if file exists if not ask user to try again
+do {
+    $filePath = Read-Host
+    $validatefile = Test-Path -Path $filePath
+    if ($validation -eq $False){
+        Write-Host -ForegroundColor Red 'File does not exist. Please try again.'
+    }
+} until ($validatefile -eq $True)
+
+# Import .CSV file
+Write-Host -ForegroundColor Yellow 'Importing .CSV file...'
+Write-Host -ForegroundColor Green 'Import complete.'
+Write-host ""

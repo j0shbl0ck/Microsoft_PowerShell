0779a9a0eab0734f070af5e121a3d43e523f42f7
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-08-20 17:28:07 -0500

    ⬆️  uploaded script

---
+++ Azure AD/O365_Message_Encryption.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This enables and brands the O365 Message Encryption feature from Microsoft.
+.NOTES
+    Author: Josh Block
+    Date: 08.20.22
+    Type: Public
+    Version: 1.0.0
+.LINK
+    https://github.com/j0shbl0ck
+    https://docs.microsoft.com/en-us/azure/information-protection/activate-service#how-to-activate-or-confirm-the-status-of-the-protection-service
+    https://docs.microsoft.com/en-us/microsoft-365/compliance/add-your-organization-brand-to-encrypted-messages?view=o365-worldwide
+#>
+
+Clear-Host
+
+# Connect to Azure Information Protection Service
+Connect-AipService
+
+# Connect to Exchange Online
+Connect-ExchangeOnline
+
+# Activate Rights Management protection service
+Enable-AipService
+
+# Customize O365 Message Encryption branding
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -BackgroundColor "#01345e" 
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -DisclaimerText "This message is confidential for the use of this addressee only."
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -EmailText "Encrypted messages sent by organization are secured by, Microsoft Purview Message Encryption" 
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -PortalText "This portal is encrypted." 
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -DisclaimerText "Encryption security disclaimer." 
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -Image ([System.IO.File]::ReadAllBytes("C:\Users\JohnDoe\Untitled.png"))
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -IntroductionText "has sent you a secure email."
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -ReadButtonText "View this message"
+
+Set-OMEConfiguration -Identity "OME Configuration" -Confirm:$false -PortalText "Organization Message Portal"

1cf6287e140aedd33c15f2bafb683019aa1cdcad
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-08-27 13:25:50 -0500

    ✨  polishing script

--- Azure AD/Exchange Online/Distribution List/Add_Members_DL_CSV/Add_Members_Distrubtion_List_CSV.ps1
+++ Azure AD/Exchange Online/Distribution List/Add_Members_DL_CSV/Add_Members_Distrubtion_List_CSV.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 07.21.22
     Type: Public
-    Version: 1.2.0
+    Version: 1.2.1
 .LINK
     https://github.com/j0shbl0ck
     https://social.technet.microsoft.com/wiki/contents/articles/54249.365-add-members-in-distribution-list-using-powershell-and-csv-list-file.aspx
@@ -20,7 +20,7 @@ Write-Host -ForegroundColor Green 'Connected to Exchange Online!'
 Write-host ""
 
 # Ask user for file path to .CSV
-Write-Host -ForegroundColor Yellow 'Please enter the path (no quotes around path) to the .CSV file:'
+Write-Host -ForegroundColor Cyan 'Please enter the path (no quotes around path) to the .CSV file:'
 # Check if file exists if not ask user to try again
 do {
     $filePath = Read-Host
@@ -37,7 +37,7 @@ Write-Host -ForegroundColor Green 'Import complete.'
 Write-host ""
 
 # Ask user for Distribution List email address
-Write-Host -ForegroundColor Yellow 'Please enter the email address (no quotes around email) of the Distribution List:'
+Write-Host -ForegroundColor Cyan 'Please enter the email address (no quotes around email) of the Distribution List:'
 $distList = Read-Host
 Write-Host -ForegroundColor Green 'Distribution List email address entered.'
 Write-host ""
@@ -47,13 +47,13 @@ ForEach ($member in $members){
     Write-Host ""
     Write-Host -ForegroundColor Yellow 'Importing member: ' $member.Name
     Add-DistributionGroupMember -Identity $distList -Member $member.ExternalEmailAddress -Confirm:$false
-    Write-Host -ForegroundColor Green 'Member added:' $member.Name
+    Write-Host -ForegroundColor Green 'Member added:' $member.ExternalEmailAddress
     Write-Host ""
 }
 
 # Get the distribution list members
 Write-Host -ForegroundColor Green 'Successfully added distribution list members:'
-Get-DistributionGroupMember -Identity $distList | Format-Table -AutoSize
+Get-DistributionGroupMember -Identity $distList | Select-Object DisplayName,PrimarySMTPAddress | Format-Table -AutoSize
 
 # Disconnect from Exchange Online
 Write-Host -ForegroundColor Yellow 'Disconnecting from Exchange Online...'

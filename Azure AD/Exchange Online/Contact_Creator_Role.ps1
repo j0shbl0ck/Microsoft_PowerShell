8e0cb96b40eec3b569f3fe7f104c3323f293d9c5
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-09 23:27:31 -0500

    🔨  squashing bugs

--- Azure AD/Exchange Online/Contact_Creator_Role.ps1
+++ Azure AD/Exchange Online/Contact_Creator_Role.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 09.09.22
     Type: Public
-    Version: 1.0.0
+    Version: 1.0.1
 .LINK
     https://github.com/j0shbl0ck
     https://practical365.com/exchange-server-role-based-access-control-in-action/#:~:text=The%20easiest%20way%20to%20create%20a%20custom%20role,Next%2C%20click%20the%20icon%20to%20add%20a%20role.
@@ -14,17 +14,6 @@
 #>
 
 Clear-Host
-
-# create function to remove all management role entries
-function Remove-AllManagementRoleEntries {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$RoleName
-    )
-    $Role = Get-ManagementRole -Name $RoleName
-    $RoleEntries = Get-ManagementRoleEntry -Role $Role
-    $RoleEntries | Remove-ManagementRoleEntry
-}
 
 # Connect to Exchange Online via Azure AD with Global/Exchange admin.
 Write-Host -ForegroundColor Cyan 'Connecting to Exchange Online...'
@@ -40,22 +29,10 @@ Write-Host -ForegroundColor Green "$roleName created successfully!`n"
 
 # Assign proper management roles to the new role.
 Write-Host -ForegroundColor Yellow "Re-assigning management roles to $roleName..."
-
-
-$roleEntries = Get-ManagementRoleEntry "$roleName\*" | Where-Object {$_.Name -notlike "*MailContact"}
-foreach ($roleEntry in $roleEntries)
-{
-    $remrole = $roleEntries.Role.tostring() + "\" +  $roleEntries.Name.tostring()
-    Remove-ManagementRoleEntry $remrole -Confirm:$false
-}
-
-
-Remove-AllManagementRoleEntries $roleName
+Get-ManagementRoleEntry "$role\*" | Where-Object {$_.Name -notlike '*MailContact'} | ForEach-Object {Remove-ManagementRoleEntry -Identity "$($_.id)\$($_.name)"}
 Write-Host -ForegroundColor Green "Management roles assigned successfully!`n"
 
 # Show the new role.
 Write-Host -ForegroundColor Magenta 'Presenting new role...'
 Get-ManagementRoleEntry "$roleName\*" | Format-List Name
 Pause
-
-Get-ManagementRoleEntry "Contact Admin\*" | Where-Object {$_.Name -notlike "*MailContact"} | Remove-ManagementRoleEntry

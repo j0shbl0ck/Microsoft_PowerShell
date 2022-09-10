20a7ef446b10a773663c7e86ec072ce230c465af
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-10 12:11:06 -0500

    🔨  added additional RBAC role

--- Azure AD/Exchange Online/Contact_Creator_Role.ps1
+++ Azure AD/Exchange Online/Contact_Creator_Role.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 09.09.22
     Type: Public
-    Version: 1.0.2
+    Version: 1.0.3
 .LINK
     https://github.com/j0shbl0ck
     https://practical365.com/exchange-server-role-based-access-control-in-action/#:~:text=The%20easiest%20way%20to%20create%20a%20custom%20role,Next%2C%20click%20the%20icon%20to%20add%20a%20role.
@@ -19,17 +19,30 @@ Write-Host -ForegroundColor Cyan 'Connecting to Exchange Online...'
 Connect-ExchangeOnline
 Clear-Host
 
-## Create a new role.
-Write-Host -ForegroundColor Yellow 'Creating a new role...'
-New-ManagementRole -Name "Contact Admin" -Description "Allows full control for creation and deletion of contacts within the Global Address List." -Parent "Mail Recipient Creation"
-Write-Host -ForegroundColor Green "Contact Admin created successfully!`n"
+Write-Host -ForegroundColor White "This script creates two custom roles, Contact Creator and Contact Modifer.`n These roles will need to be assigned to a role group such as Help Desk or a custom role group.`n"
 
-# Assign proper management roles to the new role.
-Write-Host -ForegroundColor Yellow "Re-assigning management roles to $roleName..."
-Get-ManagementRoleEntry "Contact Admin\*" | Where-Object {$_.Name -notlike '*MailContact'} | ForEach-Object {Remove-ManagementRoleEntry -Identity "$($_.id)\$($_.name)" -Confirm:$False} 
+## Create custom RBAC role - Contact Creator.
+Write-Host -ForegroundColor Yellow 'Creating Contact Creator role...'
+New-ManagementRole -Name "Contact Creator" -Description "Allows full control for creation and deletion of contacts within the Global Address List. Does not provide modification priviledges" -Parent "Mail Recipient Creation"
+Write-Host -ForegroundColor Green "Contact Creator role created successfully!`n"
+
+# Assign proper management roles to the Contact Creator role.
+Write-Host -ForegroundColor Yellow "Re-assigning management roles to Contact Creator..."
+Get-ManagementRoleEntry "Contact Creator\*" | Where-Object {$_.Name -notlike '*MailContact'} | ForEach-Object {Remove-ManagementRoleEntry -Identity "$($_.id)\$($_.name)" -Confirm:$False} 
 Write-Host -ForegroundColor Green "Management roles assigned successfully!`n"
 
-# Show the new role.
+## Create custom RBAC role - Contact Modifier.
+Write-Host -ForegroundColor Yellow 'Creating Contact Modifier role...'
+New-ManagementRole -Name "Contact Modifier" -Description "Allows full control for modification to already existent contacts within the Global Address List. Does not provide creation/deletion priviledges" -Parent "Mail Recipients"
+Write-Host -ForegroundColor Green "Contact Modifier created successfully!`n"
+
+# Assign proper management roles to the Contact Modifier role.
+Write-Host -ForegroundColor Yellow "Re-assigning management roles to Contact Modifier..."
+Get-ManagementRoleEntry "Contact Modifier\*" | Where-Object {$_.Name -notlike '*MailContact'} | ForEach-Object {Remove-ManagementRoleEntry -Identity "$($_.id)\$($_.name)" -Confirm:$False} 
+Write-Host -ForegroundColor Green "Management roles assigned successfully!`n"
+
+# Show the new roles.
 Write-Host -ForegroundColor Magenta 'Presenting new management roles...'
-Get-ManagementRoleEntry "Contact Admin\*" | Format-List Name
+Get-ManagementRoleEntry "Contact Creator\*" | Format-List Name
+Get-ManagementRoleEntry "Contact Modifier\*" | Format-List Name
 Pause

ac854b7e3c0983ce6aa672ecb7b4c96974d7ecec
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-12 10:36:58 -0500

    🔨  added report exportation and user count.

--- Azure AD/Microsoft MFA/Get_MFA_Status.ps1
+++ Azure AD/Microsoft MFA/Get_MFA_Status.ps1
@@ -3,7 +3,7 @@
     This script pulls information on whether a user has MFA enabled or not. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.1.2
+    Version: 1.1.3
     Date: 03.23.22
     Type: Public
 .NOTES
@@ -17,42 +17,47 @@ Clear-Host
 # Enter global admin credentials
 Connect-MsolService
 
-# Retrives MFA status and method per user
-Write-Host -ForegroundColor Yellow "Retrieving user count from Azure AD..."
-
 ## VARIABLES ##
-$UserCount = Get-MsolUser -All | ? { $_.UserType -ne "Guest" }
-$mfareport = [System.Collections.Generic.List[Object]]::new()
+$Users = Get-MsolUser -All | Where-Object { $_.UserType -ne "Guest" }
+$report = [System.Collections.Generic.List[Object]]::new()
 
-Get-MsolUser | 
-    Where-Object {$_.UserPrincipalName -notlike "*EXT*"} |
-    Select-Object DisplayName,UserPrincipalName,
-    @{N="MFA Status"; E={ if( $_.StrongAuthenticationRequirements.State -ne $null){ $_.StrongAuthenticationRequirements.State} else { "Disabled"}}},
-    @{N="MFA Method"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where-Object IsDefault -eq $True).MethodType} else { "None"}}} | 
-    Sort-Object DisplayName |
-    Format-Table -AutoSize
+# Finds user count in Azure Active Directory
+Write-Host -ForegroundColor Yellow "Retrieving user count from Azure AD..."
+Write-Host -ForegroundColor Green $User.Count "users found.`n"
 
-## Remove MFA from user
-#Get-MsolUser -all | Select-Object DisplayName,UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where-Object IsDefault -eq $True).MethodType} else { "Disabled"}}} | Format-Table -AutoSize | Where-Object MFAStatus -eq "Disabled" | Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationMethods @()
-#Set-MsolUser -UserPrincipalName username@your_tenant.onmicrosoft.com -StrongAuthenticationMethods @()
+# Retrieve the MFA status for each user
 
-<# # Ask if they want to export to CSV
-$export = Read-Host "Do you want to export to CSV? (y/n)"
-# if else statement
-if ($export -eq "y") {
-    # Export to CSV
-    Get-MsolUser | 
-        Where-Object {$_.UserPrincipalName -notlike "*EXT*"} |
-        Select-Object DisplayName,UserPrincipalName,
-        @{N="MFA Status"; E={ if( $_.StrongAuthenticationRequirements.State -ne $null){ $_.StrongAuthenticationRequirements.State} else { "Disabled"}}},
-        @{N="MFA Method"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where-Object IsDefault -eq $True).MethodType} else { "None"}}} | 
-        Sort-Object DisplayName |
-        Export-Csv -Path "C:\Users\username\Desktop\mfa_status.csv" -NoTypeInformation
-} elseif ($export -eq "n") {
-    Disconnect-MsolService -Confirm:$false
-    exit
-} else {
-    # Invalid input
-    Write-Host "Invalid input. Please try again."
-} #>
+ForEach ($User in $Users) {
+    $MFAEnforced = $User.StrongAuthenticationRequirements.State
+    $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber
+    $DefaultMFAMethod = ($User.StrongAuthenticationMethods | Where-Object { $_.IsDefault -eq "True" }).MethodType
+    If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) {
+        Switch ($DefaultMFAMethod) {
+            "OneWaySMS" { $MethodUsed = "One-way SMS" }
+            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" }
+            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" }
+            "PhoneAppNotification" { $MethodUsed = "Authenticator app" }
+        }
+    }
+    Else {
+        $MFAEnforced = "Not Enabled"
+        $MethodUsed = "MFA Not Used" 
+    }
+  
+    $ReportLine = [PSCustomObject] @{
+        User        = $User.UserPrincipalName
+        Name        = $User.DisplayName
+        MFAUsed     = $MFAEnforced
+        MFAMethod   = $MethodUsed 
+        PhoneNumber = $MFAPhone
+    }
+                 
+    $Report.Add($ReportLine) 
+}
+
+$desktop = [Environment]::GetFolderPath("Desktop")
+$exportPath = $desktop + "\UserMFAStatus.csv"
+Write-Host "Report is in $exportPath"
+$Report | Select-Object User, Name, MFAUsed, MFAMethod, PhoneNumber | Sort-Object Name | Out-GridView
+$Report | Sort-Object Name | Export-Csv -Path $exportPath -NoTypeInformation
 

ea511995af90933105b36df94a337f65898fc5d1
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-15 11:17:41 -0500

    ⬆️  uploaded script and new folder

---
+++ Azure AD/SharePoint Online/Remove_OneDrive_Shortcut.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    This script removes the "Add shortcuts to My Files" in SharePoint site
+.NOTES
+    Author: Josh Block
+    Date: 09.015.22
+    Type: Public
+    Version: 1.0.0
+.LINK
+    https://github.com/j0shbl0ck
+    https://www.sharepointdiary.com/2021/05/how-to-remove-add-shortcut-to-onedrive-in-sharepoint-online.html
+#>

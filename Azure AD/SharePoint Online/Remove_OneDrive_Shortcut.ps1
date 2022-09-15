450c0d84339701723d6cafe72073eeaabdd817b4
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-09-15 13:22:21 -0500

    🔨  building script

--- Azure AD/SharePoint Online/Remove_OneDrive_Shortcut.ps1
+++ Azure AD/SharePoint Online/Remove_OneDrive_Shortcut.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 09.015.22
     Type: Public
-    Version: 1.0.1
+    Version: 1.0.2
 .LINK
     https://github.com/j0shbl0ck
     https://www.sharepointdiary.com/2021/05/how-to-remove-add-shortcut-to-onedrive-in-sharepoint-online.html
@@ -13,16 +13,23 @@
 
 Clear-Host
 
-# ask user for sharepoint url
-$siteUrl = Read-Host "Enter the main SharePoint site URL"
-
-# provide warning for removal
-Write-Warning "The Add shortcut to OneDrive option will be removed from SharePoint Online document libraries. However, existing shortcuts remain in place on OneDrive." -WarningAction Inquire
-
 # Connect to SharePoint Online
 Connect-SPOService -Url $siteUrl
- 
+
+# ask user for sharepoint url
+$siteUrl = Read-Host "Enter the tenant admin SharePoint site URL (https://contoso-admin.sharepoint.com/) "
+
+# Get current state of "DisableAddShortCutsToOneDrive
+Get-SPOTenant -DisableAddShortCutsToOneDrive
+
+# provide warning for removal
+Write-Warning "The Add shortcut to OneDrive option will be removed from all SharePoint Online document libraries. However, existing shortcuts remain in place on OneDrive." -WarningAction Inquire
+
+# set DisableAddShortCutsToOneDrive to true
 Set-SPOTenant -DisableAddShortCutsToOneDrive $True
+
+# Get current state of "DisableAddShortCutsToOneDrive
+Get-SPOTenant
 
 # disconnect from SharePoint Online
 Disconnect-SPOService -Confirm:$false

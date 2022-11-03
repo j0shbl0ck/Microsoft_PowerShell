27a192847a6f749974b46b70619de0fb0669c75a
Josh Block <0go1fbn9c@relay.firefox.com>
2022-11-03 11:59:01 -0500

    ⚠️  editing script

--- Active Directory/Azure AD Connect/AAD_Connect_DeltaSync.ps1
+++ Active Directory/Azure AD Connect/AAD_Connect_DeltaSync.ps1
@@ -3,7 +3,7 @@
     This script performs a delta sync on Azure AD Connect. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.7
+    Version: 1.0.8
     Date: 02.10.22
     Type: Public
 .NOTES
@@ -12,72 +12,33 @@
 
 #>
 
+#Requires -RunAsAdministrator
+Clear-Host
 
 # ask user if they are on dc or local machine
-$userlocation = Read-Host -Prompt "Are you on the DC (where AAD Connect is hosted) [y/n]" -AsSecureString -RawInput
+$userlocation = Read-Host -Prompt "Are you on the DC (where AAD Connect is hosted) [y/n]"
 # if enters y
 if ($userlocation -eq "y") {
     # ask for what type of sync they want to perform
-    $syncType = Read-Host -Prompt "What type of sync do you want to perform? [full/delta]" -AsSecureString -RawInput
+    $syncType = Read-Host -Prompt "What type of sync do you want to perform? [full/delta]"
     # if enters full
     if ($syncType -eq "full") {
         # run full sync
-        $fullSync = Start-Process -FilePath "C:\Program Files\Azure Active Directory Connect\AzureADConnectSync.exe" -ArgumentList "-full" -Wait
-        # if full sync is successful
-        if ($fullSync.ExitCode -eq 0) {
-            # write to log
-            Write-Host "Full sync completed successfully."
-        }
-        # if full sync fails
-        else {
-            # write to log
-            Write-Host "Full sync failed."
-        }
+        Start-ADSyncSyncCycle -PolicyType Initial
     }
     # if enters delta
     elseif ($syncType -eq "delta") {
-        # ask for what type of delta sync they want to perform
-        $deltaSyncType = Read-Host -Prompt "What type of delta sync do you want to perform? [full/incremental]" -AsSecureString -RawInput
-        # if enters full
-        if ($deltaSyncType -eq "full") {
-            # run full delta sync
-            $fullDeltaSync = Start-Process -FilePath "C:\Program Files\Azure Active Directory Connect\AzureADConnectSync.exe" -ArgumentList "-full" -Wait
-            # if full delta sync is successful
-            if ($fullDeltaSync.ExitCode -eq 0) {
-                # write to log
-                Write-Host "Full delta sync completed successfully."
-            }
-            # if full delta sync fails
-            else {
-                # write to log
-                Write-Host "Full delta sync failed."
-            }
+        Start-ADSyncSyncCycle -PolicyType Delta
         }
-        # if enters incremental
-        elseif ($deltaSyncType -eq "incremental") {
-            # run incremental delta sync
-            $incrementalDeltaSync = Start-Process -FilePath "C:\Program Files\Azure Active Directory Connect\AzureADConnectSync.exe" -ArgumentList "-incremental" -Wait
-            # if incremental delta sync is successful
-            if ($incrementalDeltaSync.ExitCode -eq 0) {
-                # write to log
-                Write-Host "Incremental delta sync completed successfully."
-            }
-            # if incremental delta sync fails
-            else {
-                # write to log
-                Write-Host "Incremental delta sync failed."
-            }
-        }
-    }
-
 }
-# if enters n
-if ($userlocation -eq "[nN]") {
 
-    $userlocal = Read-Host -Prompt "Are you on a local Windows machine connected to the same network as the DC [y/n]" -AsSecureString -RawInput
+# if enters n
+if ($userlocation -eq "n") {
+
+    $userlocal = Read-Host -Prompt "Are you on a local Windows machine connected to the same network as the DC [y/n]"
 
     # if enters y
-    if ($userlocal -eq "[yY]") {
+    if ($userlocal -eq "y") {
         # check if msol module is installed
         Write-Host "Checking for MSOnline Module..."
         $msolInstalled = (Get-Module -ListAvailable -Name MSOnline)
@@ -104,12 +65,19 @@ if ($userlocation -eq "[nN]") {
         }
         # if msol module is not installed
         else {
-            "MSOnline Module not found. Would you like to install it?"
-            $installmsol = Read-Host -Prompt "Install MSOnline Module [y/n]" -AsSecureString -RawInput
+            Write-Host -ForegroundColor Yellow "MSOnline Module not found. Would you like to install it?"
+            $installmsol = Read-Host -Prompt "Install MSOnline Module [y/n]"
             # if enters y
-            if ($installmsol -eq "[yY]") {
-                Install-Module MSOnline
-                Write-Host "MSOnline Module Installed!"
+            if ($installmsol -eq "y") {
+                Write-Host -ForegroundColor Yellow "Finding AzureAD V1 PowerShell Module..."
+                $mso = "MSOnline"
+                if (-not(Get-InstalledModule -Name $mso -ErrorAction SilentlyContinue)) {
+                    Write-Host -ForegroundColor Red "${mso} Not Found. Installing ${mso}..."
+                    Install-Module -Name $mso -Force -Confirm:$False
+                    Write-Host -ForegroundColor Green "${mso} Installed!"
+                } else {
+                    Write-Host -ForegroundColor Green "${mso} Installed!"
+                }
                 # Enter global admin credentials
                 Connect-MsolService
                 # Retrives AD Connect Hostname
@@ -126,19 +94,18 @@ if ($userlocation -eq "[nN]") {
                 Exit-PSSession -Session $session
             }
             # if enters n
-            elseif ($installmsol -eq "[nN]") {
+            elseif ($installmsol -eq "n") {
                 Write-Host "User denied installation of MSOnline Module. Exiting..."
             }
         }
     }
     # if enters n
-    elseif ($userlocal -eq "[nN]") {
+    elseif ($userlocal -eq "n") {
         Write-Host "Computer not under same network or denied permissions. Exiting..."
     }
 
-}
 # if enters anything else
-else {
-    Write-Host "Invalid input. Please enter y or n."
+#else {
+    # Write-Host "Invalid input. Please enter y or n."
     # run script again
-}
+#}

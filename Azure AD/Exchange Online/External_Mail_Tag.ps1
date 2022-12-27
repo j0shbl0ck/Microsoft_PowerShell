f51eb3a1c5b8ec6132a727ab9deb08df91bcbff1
Josh Block <0go1fbn9c@relay.firefox.com>
2022-12-27 10:31:50 -0600

    🦧  updated script

--- Azure AD/Exchange Online/External_Mail_Tag.ps1
+++ Azure AD/Exchange Online/External_Mail_Tag.ps1
@@ -3,7 +3,7 @@
     This script enables or disables the external mail tag.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 12.27.22
     Type: Public
 .NOTES
@@ -15,17 +15,23 @@
 Clear-Host
 
 # ======= VARIABLES ======= #
-$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): ' 
+$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com) ' 
 # ======= VARIABLES ======= #
 
 # Connect to Exchange Online via Azure AD
 Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
 
 # Ask user if they want to enable or disable the external mail tag
-$choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (true/false): '
+$choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (enable/disable): '
 # If user input is not true or false, ask again
-while ($choice -ne 'true' -and $choice -ne 'false') {
-    $choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (true/false): '
+while ($choice -ne 'enable' -and $choice -ne 'disable') {
+    $choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (enable/disable): '
+}
+# Convert user input to boolean
+if ($choice -eq 'enable') {
+    $choice = $true
+} else {
+    $choice = $false
 }
 
 Set-ExternalInOutlook -Enabled $choice

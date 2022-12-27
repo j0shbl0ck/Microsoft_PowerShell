565930b8bce186b49ecf1909ebb4b3ccf9acf7e3
Josh Block <0go1fbn9c@relay.firefox.com>
2022-12-27 10:20:44 -0600

    🔧  uploaded new script

---
+++ Azure AD/Exchange Online/External_Mail_Tag.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    This script enables or disables the external mail tag.
+.DESCRIPTION
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 12.27.22
+    Type: Public
+.NOTES
+
+.LINK
+    Source: https://o365reports.com/2021/04/27/enable-external-email-warning-tag-in-exchange-online/
+#>
+
+Clear-Host
+
+# ======= VARIABLES ======= #
+$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): ' 
+# ======= VARIABLES ======= #
+
+# Connect to Exchange Online via Azure AD
+Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
+
+# Ask user if they want to enable or disable the external mail tag
+$choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (true/false): '
+# If user input is not true or false, ask again
+while ($choice -ne 'true' -and $choice -ne 'false') {
+    $choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (true/false): '
+}
+
+Set-ExternalInOutlook -Enabled $choice
+
+# Show the current status of the external mail tag
+Get-ExternalInOutlook
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false

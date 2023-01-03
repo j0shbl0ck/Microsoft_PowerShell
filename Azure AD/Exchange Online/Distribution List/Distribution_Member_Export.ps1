f16b17cebe2bf2036e7257489d072c34a7cfc7d1
Josh Block <0go1fbn9c@relay.firefox.com>
2023-01-03 10:21:05 -0600

    🐲  updated connection to Exchange Online and exportation

--- Azure AD/Exchange Online/Distribution List/Distribution_Member_Export.ps1
+++ Azure AD/Exchange Online/Distribution List/Distribution_Member_Export.ps1
@@ -3,7 +3,7 @@
     Export distrubtion group with members in CSV. 
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 01.04.22
     Type: Public
 .NOTES
@@ -16,23 +16,26 @@
 
 
 # ======= VARIABLES ======= #
-$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com) ' 
+$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com)' 
 # ======= VARIABLES ======= #
 
 # Connect to Exchange Online via Azure AD
 Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
 
 # Get Distrubution Group Email
-$groupname = Read-Host -Prompt "Enter the email of the distrubution group"
+$groupemail = Read-Host -Prompt "Enter the email of the distrubution group"
 
 # Exportation section of the script
-Write-Host -ForegroundColor Yellow "Creating ${groupname} CSV file..."
-Get-DistributionGroupMember -Identity $groupname | Select-Object Name, PrimarySmtpAddress | Export-CSV "$($env:USERPROFILE)\Desktop\$groupname.csv"  -NoTypeInformation -ErrorAction Stop
+Write-Host -ForegroundColor Yellow "Creating $groupname CSV file..."
+$groupname = Get-DistributionGroup -Identity $groupemail | Select-Object Name
+Get-DistributionGroupMember -Identity $groupemail | Select-Object Name, PrimarySmtpAddress | Export-CSV "$($env:USERPROFILE)\Desktop\$groupname.csv"  -NoTypeInformation -ErrorAction Stop
+
 Write-Host -ForegroundColor Green "Complete! Document found at Desktop\${groupname}.csv"
+
 
 # Terminates Exchange Online PS Session
 Write-Host 'Terminating Exchange Online PS Session...' -ForegroundColor Cyan
 Write-Host ""
 Disconnect-ExchangeOnline -Confirm:$false
 
-Pause
+Break

fd37e892b588b177adc4d15bfe34e69e65cf0d2d
Josh Block <0go1fbn9c@relay.firefox.com>
2023-01-03 11:05:15 -0600

    🫁  added csv header check

--- Azure AD/Exchange Online/Mail-enabled Security/MES_User_Import.ps1
+++ Azure AD/Exchange Online/Mail-enabled Security/MES_User_Import.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 01.03.23
     Type: Public
-    Version: 1.0.1
+    Version: 1.0.2
 .LINK
     https://github.com/j0shbl0ck
     https://social.technet.microsoft.com/wiki/contents/articles/54249.365-add-members-in-distribution-list-using-powershell-and-csv-list-file.aspx
@@ -38,42 +38,53 @@ Write-host ""
 # Set Distribution List CSV variable
 $users = Import-CSV $filePath
 
-# Ask user if they want to create a distribution list or already have one
-while ($choice -ne 'y' -and $choice -ne 'n') 
-{
-    $choice = Read-Host -Prompt 'Do you want to create the Mail-enabled Security group? (Y/N)'
-    if ($choice -eq 'y') {
-        # ask user for the name of the distribution list
-        Write-Host -ForegroundColor Yellow 'What is the name of the Mail-enabled Security group?'
-        $dlname = Read-Host
-        # ask user for the email address of the distribution list
-        Write-Host -ForegroundColor Yellow 'What is the email address of the Mail-enabled Security group?'
-        $dlemail = Read-Host
-        # create the distribution list
-        New-DistributionGroup -Name $dlname -PrimarySmtpAddress $dlemail -Type "Security"
-        Write-Host -ForegroundColor Green 'Mail-enabled Security group: ' + $dlname + ' created.'
-        Write-host ""
-        # Perform the create distrubution list operation
-        ForEach ($user in $users){
-            Write-Host -ForegroundColor Yellow 'Adding users to Mail-enabled Security group: ' $_.Email
-            Add-DistributionGroupMember -Identity "$dlname" -Member $user.Email
-            Write-Host -ForegroundColor Green 'User: ' + $user.Email + ' added.'
+# get headers from CSV file
+$headers = $users | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+
+# check if the CSV file has the correct headers
+if ($headers -notcontains 'Email') {
+    Write-Host -ForegroundColor Red 'The CSV file does not contain the correct headers. Please correct headers and try again.'
+    Write-Host -ForegroundColor Red 'The headers should be: Name,Email'
+    Write-host ""
+    exit
+}
+else {
+    # Ask user if they want to create a distribution list or already have one
+    while ($choice -ne 'y' -and $choice -ne 'n') 
+    {
+        $choice = Read-Host -Prompt 'Do you want to create the Mail-enabled Security group? (Y/N)'
+        if ($choice -eq 'y') {
+            # ask user for the name of the distribution list
+            Write-Host -ForegroundColor Yellow 'What is the name of the Mail-enabled Security group?'
+            $dlname = Read-Host
+            # ask user for the email address of the distribution list
+            Write-Host -ForegroundColor Yellow 'What is the email address of the Mail-enabled Security group?'
+            $dlemail = Read-Host
+            # create the distribution list
+            New-DistributionGroup -Name $dlname -PrimarySmtpAddress $dlemail -Type "Security"
+            Write-Host -ForegroundColor Green 'Mail-enabled Security group: ' + $dlname + ' created.'
+            Write-host ""
+            # Perform the create distrubution list operation
+            ForEach ($user in $users){
+                Write-Host -ForegroundColor Yellow 'Adding users to Mail-enabled Security group: ' $_.Email
+                Add-DistributionGroupMember -Identity "$dlname" -Member $user.Email
+                Write-Host -ForegroundColor Green 'User: ' + $user.Email + ' added.'
+            }
+        } elseif ($choice -eq 'n') {
+            # get the email of the distribution list
+            Write-Host -ForegroundColor Yellow 'What is the email address of the Mail-enabled Security group?'
+            $dlemail2 = Read-Host
+            # Perform the create distrubution list operation
+            ForEach ($user in $users){
+                Write-Host -ForegroundColor Yellow 'Adding users to Mail-enabled Security group: ' $_.Email
+                Add-DistributionGroupMember -Identity "$dlemail2" -Member $user.Email
+                Write-Host -ForegroundColor Green 'User: ' + $user.Email + ' added.'
+            }
+        } else {
+            Write-Host -ForegroundColor Red "Invalid input. Please type 'y' or 'n'"
         }
-    } elseif ($choice -eq 'n') {
-        # get the email of the distribution list
-        Write-Host -ForegroundColor Yellow 'What is the email address of the Mail-enabled Security group?'
-        $dlemail2 = Read-Host
-        # Perform the create distrubution list operation
-        ForEach ($user in $users){
-            Write-Host -ForegroundColor Yellow 'Adding users to Mail-enabled Security group: ' $_.Email
-            Add-DistributionGroupMember -Identity "$dlemail2" -Member $user.Email
-            Write-Host -ForegroundColor Green 'User: ' + $user.Email + ' added.'
-        }
-    } else {
-        Write-Host -ForegroundColor Red "Invalid input. Please type 'y' or 'n'"
     }
 }
-
 
 # Disconnect from Exchange Online
 Write-Host -ForegroundColor Yellow 'Disconnecting from Exchange Online...'

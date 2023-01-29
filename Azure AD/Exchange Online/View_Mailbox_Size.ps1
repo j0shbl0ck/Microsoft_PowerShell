800cb920c8cea7f6187856692e9d34d6a143cebd
Josh Block <0go1fbn9c@relay.firefox.com>
2023-01-29 16:43:23 -0600

    🏠  update script

--- Azure AD/Exchange Online/View_Mailbox_Size.ps1
+++ Azure AD/Exchange Online/View_Mailbox_Size.ps1
@@ -1,25 +1,34 @@
 <#
 .SYNOPSIS
-    This script allows you to view all mailbox sizes in your tenant.
+This script allows you to view all mailbox sizes in your tenant.
 .DESCRIPTION
-    Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
-    Date: 12.05.22
-    Type: Public
+Author: j0shbl0ck https://github.com/j0shbl0ck
+Version: 1.0.1
+Date: 12.05.22
+Type: Public
 .NOTES
 
 .LINK
-    Source: https://m365scripts.com/exchange-online/get-mailbox-details-in-microsoft-365-using-powershell/
+Source: https://m365scripts.com/exchange-online/get-mailbox-details-in-microsoft-365-using-powershell/
 #>
 
 Clear-Host
 
-# ======= VARIABLES ======= #
-$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): ' 
-# ======= VARIABLES ======= #
+$gAdmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): '
 
-# Connect to Exchange Online via Azure AD
-Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
+#Connect to Exchange Online via Azure AD
+Connect-ExchangeOnline -UserPrincipalName $gAdmin | Out-Null
 
+Write-Output "Fetching mailbox details... Please wait..."
+$mailboxes = Get-Mailbox -ResultSize Unlimited
 
-Get-Mailbox -ResultSize Unlimited | ForEach-Object { Get-MailboxStatistics -identity $_.userprincipalName | Select-Object Displayname,TotalItemSize | Format-Table Displayname,TotalItemSize}| Format-Table Displayname,TotalItemSize
+#Calculate mailbox size and select relevant properties
+$mailboxSizes = $mailboxes | ForEach-Object {
+Get-MailboxStatistics -Identity $_.UserPrincipalName |
+Select-Object DisplayName, TotalItemSize
+}
+
+#Format and display results
+$mailboxSizes | Format-Table -AutoSize DisplayName, TotalItemSize | Out-String | Write-Output
+
+Write-Output "Mailbox details retrieved successfully."

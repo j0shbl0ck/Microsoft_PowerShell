15ca90867cc856c0fd2be61453c8ef23ce77684e
Josh Block <0go1fbn9c@relay.firefox.com>
2023-02-17 23:10:47 -0600

    🎄  updated script

--- Azure AD/SharePoint Online/Remove_OneDrive_Shortcut.ps1
+++ Azure AD/SharePoint Online/Remove_OneDrive_Shortcut.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 09.15.22
     Type: Public
-    Version: 1.0.3
+    Version: 1.0.4
 .LINK
     https://github.com/j0shbl0ck
     https://www.sharepointdiary.com/2021/05/how-to-remove-add-shortcut-to-onedrive-in-sharepoint-online.html
@@ -13,17 +13,41 @@
 
 Clear-Host
 
-# ask user for sharepoint url
-$siteUrl = Read-Host "Enter the tenant admin SharePoint site URL (https://contoso-admin.sharepoint.com/) "
+# Prompt for SharePoint Online site URL and validate input
+do {
+    $siteUrl = Read-Host "Enter the tenant admin SharePoint site URL (https://contoso-admin.sharepoint.com/) "
+} until ($siteUrl -match '^https?://[-\w.]+(:\d+)?/')
 
-# Connect to SharePoint Online
-Connect-SPOService -Url $siteUrl
+# Connect to SharePoint Online and handle errors
+try {
+    Connect-SPOService -Url $siteUrl -ErrorAction Stop
+} catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+    Exit 1
+}
 
-# set DisableAddShortCutsToOneDrive to true
-Set-SPOTenant -DisableAddShortCutsToOneDrive $True
+# Prompt for password policy option and handle errors
+do {
+    $policy = Read-Host -Prompt 'Set shortcut policy (D = DisableAddShortCutsToOneDrive, E = EnableAddShortCutsToOneDrive)'
+} until ($policy -in 'D', 'E')
 
-# Let user know script is done
-Write-Host "`nDisabling of adding shortcuts to OneDrive is set to TRUE" -ForegroundColor Green
+try {
+    switch ($policy) {
+        'D' { Set-SPOTenant -DisableAddShortCutsToOneDrive $True -ErrorAction Stop }
+        'E' { Set-SPOTenant -DisableAddShortCutsToOneDrive $False -ErrorAction Stop }
+    }
+} catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+    Exit 1
+}
 
-# disconnect from SharePoint Online
-Disconnect-SPOService 
+# Show confirmation of shortcut policy
+Write-Host "`nDisabling of adding shortcuts to OneDrive is set to $($policy -eq 'D')" -ForegroundColor Green
+
+# Disconnect from SharePoint Online and handle errors
+try {
+    Disconnect-SPOService -ErrorAction Stop
+} catch {
+    Write-Host "Error: $_" -ForegroundColor Red
+    Exit 1
+}

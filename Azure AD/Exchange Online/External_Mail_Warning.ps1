b3b24e065291a282d42a38d67bbfd0a913f6c6d9
Josh Block <0go1fbn9c@relay.firefox.com>
2023-03-30 14:36:07 -0500

    🥰  added more code

--- Azure AD/Exchange Online/External_Mail_Warning.ps1
+++ Azure AD/Exchange Online/External_Mail_Warning.ps1
@@ -3,7 +3,7 @@
     This script auto creates a warning message for external messages.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 03.30.23
     Type: Public
 .EXAMPLE
@@ -21,3 +21,15 @@ $gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain
 # Connect to Exchange Online via Azure AD
 Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
 
+# Define the parameters
+$Name = "External Email Disclaimer"
+$Priority = 0
+$Enabled = $true
+$FromScope = "NotInOrganization"
+$SentToScope = "OutsideOrganization"
+$ApplyHtmlDisclaimer = $true
+$ApplyHtmlDisclaimerText = '<div style="background-color: rgb(51, 56, 0) !important; width: 100%; border-style: solid; border-color: rgb(90, 67, 0); border-width: 1pt; padding: 2pt; font-size: 10pt; line-height: 12pt; font-family: Calibri; color: rgb(255, 255, 255) !important; text-align: left; margin-bottom: 2pt;" data-ogsc="black" data-ogsb="rgb(255, 199, 206)"><span style="color: rgb(255, 212, 0) !important; font-weight: bold;" data-ogsc="rgb(90, 67, 0)">WARNING:</span> This message has originated from outside the organization. Please use proper judgment and caution when opening attachments, clicking links, or responding to this email.</div>'
+$ApplyDisclaimerFallbackAction = "Wrap"
+
+# Create the transport rule
+New-TransportRule -Name $Name -Priority $Priority -Enabled $Enabled -FromScope $FromScope -SentToScope $SentToScope -ApplyHtmlDisclaimer $ApplyHtmlDisclaimer -ApplyHtmlDisclaimerText $ApplyHtmlDisclaimerText -ApplyDisclaimerFallbackAction $ApplyDisclaimerFallbackAction

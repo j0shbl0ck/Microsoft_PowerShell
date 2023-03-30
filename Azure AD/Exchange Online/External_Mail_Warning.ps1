0068862b75c8b7498b45b4f4e2405ca19429d2b4
Josh Block <0go1fbn9c@relay.firefox.com>
2023-03-30 14:48:46 -0500

    🦄  updated disclaimer location

--- Azure AD/Exchange Online/External_Mail_Warning.ps1
+++ Azure AD/Exchange Online/External_Mail_Warning.ps1
@@ -3,7 +3,7 @@
     This script auto creates a warning message for external messages.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.3
+    Version: 1.0.4
     Date: 03.30.23
     Type: Public
 .EXAMPLE
@@ -28,7 +28,7 @@ $Enabled = $true
 $FromScope = "NotInOrganization"
 $SentToScope = "InOrganization"
 $ApplyHtmlDisclaimerText = '<div style="background-color: rgb(51, 56, 0) !important; width: 100%; border-style: solid; border-color: rgb(90, 67, 0); border-width: 1pt; padding: 2pt; font-size: 10pt; line-height: 12pt; font-family: Calibri; color: rgb(255, 255, 255) !important; text-align: left; margin-bottom: 2pt;" data-ogsc="black" data-ogsb="rgb(255, 199, 206)"><span style="color: rgb(255, 212, 0) !important; font-weight: bold;" data-ogsc="rgb(90, 67, 0)">WARNING:</span> This message has originated from outside the organization. Please use proper judgment and caution when opening attachments, clicking links, or responding to this email.</div>'
-$ApplyHtmlDisclaimerLocation = "Append"
+$ApplyHtmlDisclaimerLocation = "Prepend"
 $ApplyHtmlDisclaimerFallbackAction = "Wrap"
 
 # Create the transport rule

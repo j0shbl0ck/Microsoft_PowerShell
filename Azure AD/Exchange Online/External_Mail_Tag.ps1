e9dcd6a83ed5fd838a0d60236fad01d1e0b01b3d
Josh Block <0go1fbn9c@relay.firefox.com>
2023-05-25 10:49:31 -0500

    🍜  updated script

--- Azure AD/Exchange Online/External_Mail_Tag.ps1
+++ Azure AD/Exchange Online/External_Mail_Tag.ps1
@@ -3,7 +3,7 @@
     This script enables or disables the external mail tag.
 .DESCRIPTION
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 12.27.22
     Type: Public
 .NOTES
@@ -12,29 +12,33 @@
     Source: https://o365reports.com/2021/04/27/enable-external-email-warning-tag-in-exchange-online/
 #>
 
+# Clear the host screen
 Clear-Host
 
-# ======= VARIABLES ======= #
-$gadmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com) ' 
-# ======= VARIABLES ======= #
+# Variables
+$globalAdminUPN = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com)'
 
 # Connect to Exchange Online via Azure AD
-Connect-ExchangeOnline -UserPrincipalName $gadmin | Clear-Host
+Connect-ExchangeOnline -UserPrincipalName $globalAdminUPN | Clear-Host
 
-while ($choice -ne 'y' -and $choice -ne 'n') 
-{
-    # Ask user if they want to enable or disable the external mail tag
-    $choice = Read-Host -Prompt 'Do you want to enable or disable the external mail tag? (enable/disable)'
-    if ($choice -eq 'enable') {
-        $choice = $true
-    } elseif ($choice -eq 'disable') {
-        $choice = $false
-    } else {
+# Prompt for enabling or disabling external mail tag
+$validChoices = @('enable', 'disable')
+$choice = $null
+
+while ($choice -notin $validChoices) {
+    $choice = Read-Host -Prompt "Do you want to enable or disable the external mail tag? (Type 'enable' or 'disable')"
+    if ($choice -notin $validChoices) {
         Write-Host -ForegroundColor Red "Invalid input. Please type 'enable' or 'disable'"
     }
 }
 
-Set-ExternalInOutlook -Enabled $choice
+if ($choice -eq 'enable') {
+    # Set external mail tag if enabled
+    Set-ExternalInOutlook -Enabled $true
+} else {
+    # Disable external mail tag if disabled
+    Set-ExternalInOutlook -Enabled $false
+}
 
 # Show the current status of the external mail tag
 Get-ExternalInOutlook

a892f302d1342494b4873c64394e1b05413e61d5
Josh Block <0go1fbn9c@relay.firefox.com>
2023-05-25 10:40:54 -0500

    🤩  updating script

--- Azure AD/Exchange Online/View_Mailbox_Size.ps1
+++ Azure AD/Exchange Online/View_Mailbox_Size.ps1
@@ -3,7 +3,7 @@
 This script allows you to view all mailbox sizes in your tenant.
 .DESCRIPTION
 Author: j0shbl0ck https://github.com/j0shbl0ck
-Version: 1.0.1
+Version: 1.0.2
 Date: 12.05.22
 Type: Public
 .NOTES
@@ -14,7 +14,7 @@ Source: https://m365scripts.com/exchange-online/get-mailbox-details-in-microsoft
 
 Clear-Host
 
-$gAdmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): '
+$gAdmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com)'
 
 #Connect to Exchange Online via Azure AD
 Connect-ExchangeOnline -UserPrincipalName $gAdmin | Out-Null
@@ -32,3 +32,32 @@ Select-Object DisplayName, TotalItemSize
 $mailboxSizes | Format-Table -AutoSize DisplayName, TotalItemSize | Out-String | Write-Output
 
 Write-Output "Mailbox details retrieved successfully."
+
+# Clear the host screen
+Clear-Host
+
+# Prompt for Global/Exchange Admin UPN
+$gAdmin = Read-Host -Prompt 'Input Global/Exchange Admin UPN (globaladmin@domain.com): '
+
+try {
+    # Connect to Exchange Online via Azure AD
+    Connect-ExchangeOnline -UserPrincipalName $gAdmin -ErrorAction Stop | Out-Null
+
+    Write-Output "Fetching mailbox details... Please wait..."
+
+    # Fetch mailbox details
+    $mailboxes = Get-Mailbox -ResultSize Unlimited
+
+    # Fetch mailbox sizes and select relevant properties
+    $mailboxSizes = $mailboxes | ForEach-Object {
+        Get-MailboxStatistics -Identity $_.UserPrincipalName |
+        Select-Object DisplayName, TotalItemSize
+    }
+
+    # Format and display results
+    $mailboxSizes | Format-Table -AutoSize DisplayName, TotalItemSize | Out-String | Write-Output
+
+    Write-Output "Mailbox details retrieved successfully."
+} catch {
+    Write-Host -ForegroundColor Red "An error occurred: $($_.Exception.Message)"
+}

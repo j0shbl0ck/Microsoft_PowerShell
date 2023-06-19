f74d1a7b7ed619f071a02d4ccbd66a95c1e88cff
Josh Block <0go1fbn9c@relay.firefox.com>
2023-06-19 08:59:18 -0500

    🦖  revamped entire script

--- Active Directory/Azure AD Connect/ADSync_Service.bat
+++ Active Directory/Azure AD Connect/ADSync_Service.bat
@@ -1,6 +1,20 @@
 :: This script stops and starts the service, ADSync for Azure AD Connect
 :: This script is intended to be run as a scheduled task
-:: 11.28.2022
+:: 06.19.2023 - Last Updated
 
-net stop ADSync
-net start ADSync
+@echo off
+setlocal
+
+set "ServiceName=ADSync"
+
+REM Check if the service is running
+sc query "%ServiceName%" | findstr /i "STATE.*RUNNING" >nul
+
+if %errorlevel% equ 0 (
+    echo The "%ServiceName%" service is already running.
+) else (
+    echo Starting the "%ServiceName%" service...
+    net start "%ServiceName%"
+)
+
+endlocal

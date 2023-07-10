befa19cd54500349b0d381abaf12da10c048ac1c
Josh Block <0go1fbn9c@relay.firefox.com>
2023-07-10 10:14:56 -0500

    🐉  added loop

--- Azure AD/Exchange Online/Distribution List/Send_As_Delegate_DL.ps1
+++ Azure AD/Exchange Online/Distribution List/Send_As_Delegate_DL.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 07.10.23
     Type: Public
-    Version: 1.0.2
+    Version: 1.0.3
 .LINK
     https://github.com/j0shbl0ck
     https://forums.powershell.org/t/the-operation-couldnt-be-performed-because-user-matches-multiple-entries/16194
@@ -15,22 +15,34 @@
 # Connect to Exchange Online via Azure AD
 Connect-ExchangeOnline | Clear-Host
 
-# Get the Distribution List Object Id
-$DL = Read-Host -Prompt "Enter the Distribution List Email Address"
-$GUID = Get-DistributionGroup -Identity $DL | Select-Object GUID
+# Start a loop
+Do {
 
-# Get the User Email Address
-$User = Read-Host -Prompt "Enter the User Email Address to add Send As permissions to the Distribution List"
+    # Get the Distribution List Object Id
+    $DL = Read-Host -Prompt "Enter the Distribution List Email Address"
+    $GUID = Get-DistributionGroup -Identity $DL | Select-Object GUID
 
-# Add the Send As permissions to the Distribution List
-Try {
-    Add-RecipientPermission -Identity $GUID -Trustee $User -AccessRights SendAs
-    Write-Host "Send As permissions successfully added to the Distribution List."
-} Catch {
-    Write-Host "An error occurred while adding Send As permissions to the Distribution List."
-    Write-Host $Error[0].Exception.Message
-}
+    # Get the User Email Address
+    $User = Read-Host -Prompt "Enter the User Email Address to add Send As permissions to the Distribution List"
 
-# Verify the Send As permissions were added to the Distribution List
-Get-RecipientPermission -Identity $GUID
+    # Add the Send As permissions to the Distribution List
+    Try {
+        Add-RecipientPermission -Identity $GUID -Trustee $User -AccessRights SendAs
+        Write-Host "Send As permissions successfully added to the Distribution List."
+    } Catch {
+        Write-Host "An error occurred while adding Send As permissions to the Distribution List."
+        Write-Host $Error[0].Exception.Message
+    }
+
+    # Ask the user if they want to add another user
+    $Continue = Read-Host -Prompt "Would you like to add another user? (Y/N)"
+
+    # Check for invalid input
+    If ($Continue -ne "Y") {
+        Write-Host "Exiting script."
+        Exit
+    }
+
+} While ($Continue -eq "Y")
+
 

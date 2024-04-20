ab483fceeccc71a3d1481a83a0046148760886bb
Josh Block <0go1fbn9c@relay.firefox.com>
2024-04-20 00:39:31 -0500

    🐇  uploaded script

--- Active Directory/Azure AD Connect/ImmutableID/Clear_Immutable_ID copy.ps1
+++ Active Directory/Azure AD Connect/ImmutableID/Clear_Immutable_ID copy.ps1
@@ -13,6 +13,9 @@
 #>
 
 # Check for if Microsoft Graph module is downloaded
+# Check for if Microsoft Graph module is downloaded
+Function Block-Script {
+}
 
 # Clear any Microsoft Graph connections then prompting for sign-in
 #Disconnect-MgGraph -ErrorAction SilentlyContinue
@@ -21,20 +24,48 @@
 #Connect-MgGraph -Scopes "User.ReadWrite.All"
 
 # Pull all users within tenant
-$entrausers = Get-Mguser -ConsistencyLevel:eventual -Count:userCount -Filter "endsWith(UserPrincipalName, '@allstate75.com')" | Select DisplayName,UserPrincipalName,Id
+$entrausers = Get-Mguser -ConsistencyLevel:eventual -Count:userCount -Filter "endsWith(UserPrincipalName, '@allstate75.com')" | Select DisplayName,UserPrincipalName,Id,OnPremisesImmutableId
 
 
 ForEach ($entrauser in $entrausers){
     
     # Get User's DisplayName
-    Get-MgUser -UserId $entrauser.Id -Property DisplayName | select DisplayName
-    $upn = $entrauser.UserPrincipalName;
+    $dp = $entrauser.DisplayName
+
+    # Get user's UPN
+    $upn = $entrauser.UserPrincipalName
+    
+    # Get user's ImmutableID
+    $iis = Get-Mguser -UserId $entrauser.id -Property onPremisesImmutableId | select onpremisesimmutableid
+    $opii = $iis.OnPremisesImmutableId
+        if ($opii -eq $null) {
+            Write-Host -ForegroundColor Magenta "Current ImmutableID for ${dp}: null"
+        } else {
+            Write-Host -ForegroundColor Yellow "Current ImmutableID for ${dp}: ${opii}"
+        }
+
+    
+    # Display Current ImmutableID for user
+    #Write-Host -ForegroundColor Yellow "Current ImmutableID for ${dp}: ${opii}"
+
+    ForEach ($ii in $iis){
+
     # Clear the ImmutableID value for user
     Invoke-MgGraphRequest -Method PATCH -Uri "https://graph.microsoft.com/v1.0/users/${upn}" -Body @{OnPremisesImmutableId = $null}
-    # Get User's Immutable ID value
-    Get-Mguser -UserId $entrauser.Id -Property onPremisesImmutableId | select onpremisesimmutableid
+    
+    # Display Updated ImmutableID for user
+    $iis2 = Get-Mguser -UserId $entrauser.id -Property onPremisesImmutableId | select onpremisesimmutableid
+    $opii2 = $iis2.OnPremisesImmutableId
+        if ($opii2 -eq $null) {
+            Write-Host -ForegroundColor Green "Current ImmutableID for ${dp}: null"
+        } else {
+            Write-Host -ForegroundColor Red "Current ImmutableID for ${dp}: ${opii2}"
+        }
+    Write-Host ""
+
+    }
 
 }
 
 # Disconnect from Microsoft Graph module
-#Disconnect-MgGraph
+Disconnect-MgGraph

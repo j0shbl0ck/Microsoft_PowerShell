7b64530b60694df0747d5ea942cb9500b4584a6a
Josh Block <0go1fbn9c@relay.firefox.com>
2024-08-21 22:25:57 -0500

    😍 new script

---
+++ Azure AD/Exchange Online/smtptest.ps1
@@ -0,0 +1,9 @@
+$EmailFrom = "xlock@test.com"
+$EmailTo = "2lock@test2.com"  
+$Subject = "today date"
+$Body = "TODAY SYSTEM DATE=01/04/2016  SYSTEM TIME=11:32:05.50"
+$SMTPServer = "smtp.x.com"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)   
+$SMTPClient.EnableSsl = $true    
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("user", "password")    
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)

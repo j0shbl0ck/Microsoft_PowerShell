a53fd41899ceb7917aa9de8f04e7a8ba9f048790
Josh Block <0go1fbn9c@relay.firefox.com>
2025-04-29 09:56:01 -0500

    👽  updated script

--- Azure AD/Exchange Online/access_mailbox.ps1
+++ Azure AD/Exchange Online/access_mailbox.ps1
@@ -5,7 +5,7 @@
     Author: Josh Block
     Date: 04.28.25
     Type: Public
-    Version: 1.0.0
+    Version: 1.0.1
 .LINK
     https://github.com/j0shbl0ck
     https://www.reddit.com/r/Office365/comments/1k5xu5b/office_365_global_admin_hacked_with_mfa_enabled
@@ -30,16 +30,68 @@ Function Connect_Exo{
     }   
 }
 
+# Get the mailbox to connect to
+$mailbox = Read-Host -Prompt "Enter the mailbox to access:"
+
+# Get user connecting to the mailbox
+$user = Read-Host -Prompt "Enter user accessing the mailbox:"
+
+Function Connect_Mailbox{
+    try {
+        # Add the user to the mailbox
+        Add-MailboxPermission -Identity $mailbox -User $user -AccessRights FullAccess -InheritanceType All
+        Write-Host -ForegroundColor Green "Added $user to $mailbox."
+
+        # Output mailbox URL acess
+        $mailboxUrl = "https://outlook.office.com/mail/$mailbox"
+        Write-Host -ForegroundColor Green "Access the mailbox at: $mailboxUrl"
+    } catch {
+        Write-Host "Failed to add user to mailbox. Ending script." -ForegroundColor Red
+        exit
+    }
+}
+
+Function Disconnect_Mailbox{
+    try {
+        # Ask user to type "disconnect" to remove mailbox access. allow upper and lowercase
+        $disconnect = Read-Host -Prompt "Type 'disconnect' to remove mailbox access:"
+        if ($disconnect -eq "disconnect" -or $disconnect -eq "Disconnect") {
+            # Get the mailbox to disconnect from
+            $mailbox = Read-Host -Prompt "Enter the mailbox to disconnect from:"
+
+            # Get user to remove from the mailbox
+            $user = Read-Host -Prompt "Enter user to remove from the mailbox:"
+
+            # Remove the user from the mailbox
+            Remove-MailboxPermission -Identity $mailbox -User $user -AccessRights FullAccess -InheritanceType All
+            Write-Host -ForegroundColor Green "Removed $user from $mailbox."
+        } else {
+            Write-Host "Invalid input. Ending script." -ForegroundColor Red
+            exit
+        }
+    } catch {
+        Write-Host "Failed to remove user from mailbox. Ending script." -ForegroundColor Red
+        exit
+    }
+}
+
+Function Diconnect_Exo{
+    try {
+        Write-Host -ForegroundColor Yellow 'Disconnecting from Exchange Online...'
+        
+        # Disconnect from Exchange Online
+        Disconnect-ExchangeOnline -Confirm:$false | Clear-Host
+        
+        Write-Host -ForegroundColor Green 'Disconnected from Exchange Online.'
+        Write-Host ""
+    }
+    catch {
+        Write-Host "Failed to disconnect from Exchange Online. Ending script." -ForegroundColor Red
+        exit
+    }   
+}
+
 Connect_Exo
-
-Use the following command to grant the global admin access to the target mailbox:
-
-Add-MailboxPermission -Identity user@domain.com -User gadmin@domain.com -AccessRights FullAccess -InheritanceType All
-
-While logged into the global admin account, use this URL to open the mailbox:
-
-https://outlook.office.com/mail/user@domain.com
-
-When finished, use the following command to remove the access permissions:
-
-Remove-MailboxPermission -Identity user@domain.com -User gadmin@domain.com -AccessRights FullAccess -InheritanceType All
+Connect_Mailbox
+Disconnect_Mailbox
+Diconnect_Exo

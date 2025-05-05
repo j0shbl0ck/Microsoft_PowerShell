e8a1fee354489d0dafab7fa988a4886d985c81eb
Josh Block <0go1fbn9c@relay.firefox.com>
2025-05-05 10:34:58 -0500

    🧭  updated script

--- Scripts/Windows_System/update_windows.ps1
+++ Scripts/Windows_System/update_windows.ps1
@@ -1,2 +1,12 @@
+<#
+.SYNOPSIS
+    This script installs PSWindowsUpdate module and installs all available Windows updates.
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 05.05.25
+    Type: Public
+.NOTES
+
+#>
 Install-Module -Name PSWindowsUpdate -Confirm:$false
 Install-WindowsUpdate -AcceptAll -IgnoreReboot

720af4f341a0e67e46a3bad6abd4188ad4d6c2e4
Josh Block <0go1fbn9c@relay.firefox.com>
2025-06-17 21:24:18 -0500

    ⚠️  incomplete script

---
+++ Azure AD/Exchange Online/Restore_Mailbox/Restore_Archive_Deleted.ps1
@@ -0,0 +1,15 @@
+
+https://stackoverflow.com/questions/68106483/recover-items-in-a-users-online-archive-from-there-recoverable-items
+
+
+Get-Mailbox -Identity user@domain.com -RecipientTypeDetails UserMailbox | Select-Object UserPrincipalName,ExchangeGuid
+
+$ArchiveGuid = (Get-Mailbox -Identity user@domain.com -RecipientTypeDetails UserMailbox).ArchiveGuid
+
+Write-Host $ArchiveGuid
+
+
+Restore-RecoverableItems -Identity "$GuidSource" -ResultSize "unlimited" -SourceFolder RecoverableItems
+
+
+Get-RecoverableItems -identity "$GuideSource" | Restore-RecoverableItems
